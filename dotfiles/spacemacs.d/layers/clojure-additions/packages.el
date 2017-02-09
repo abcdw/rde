175ba2c91423af7c97d598ab5a23a5e6e59d01f0
abcdw <andrewtropin@gmail.com>
2017-02-09 12:34:58 +0300

    Add hotkeys for parinfer toggle and lispy comment

--- dotfiles/spacemacs.d/layers/clojure-additions/packages.el
+++ dotfiles/spacemacs.d/layers/clojure-additions/packages.el
@@ -12,12 +12,11 @@
 ;;; Code:
 
 (defconst clojure-additions-packages
-  '(lispy
+  '((lispy (recipe :fetcher github :repo "abo-abo/lispy"))
     ;; evil-lispy
     parinfer
-    (sayid :location (recipe
-                      :fetcher github
-                      :repo "bpiel/sayid"))))
+    (sayid :location (recipe :fetcher github :repo "bpiel/sayid"))))
+
 
 (defun clojure-additions/init-sayid ()
   (use-package sayid :defer t))
@@ -36,20 +35,25 @@
             '(defaults       ; should be included.
                pretty-parens  ; different paren styles for different modes.
                evil           ; If you use Evil.
-               lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-               paredit        ; Introduce some paredit commands.
-               smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-               smart-yank))   ; Yank behavior depend on mode.
+               lispy))          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+               ;; paredit ; Introduce some paredit commands.
+      ;; smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+      ;; smart-yank
+                                        ; Yank behavior depend on mode.
+      (setq parinfer-lighters '("P:Ind" . "P:Par"))
       (add-hook 'clojure-mode-hook #'parinfer-mode)
       (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
       (add-hook 'common-lisp-mode-hook #'parinfer-mode)
       (add-hook 'scheme-mode-hook #'parinfer-mode)
       (add-hook 'lisp-mode-hook #'parinfer-mode))
-    ;; :bind
-    ;; (("C-," . parinfer-toggle-mode))
 
     :config
     (progn
-      (define-key evil-insert-state-map (kbd "C-.") 'parinfer-toggle-mode))))
+      (define-key evil-insert-state-map (kbd "C-.") 'parinfer-toggle-mode)
+      (define-key evil-normal-state-map (kbd "C-.") 'parinfer-toggle-mode)
+      (define-key evil-insert-state-map (kbd "C-;") 'lispy-comment)
+      (define-key evil-normal-state-map (kbd "C-;") 'lispy-comment))))
+
+
 
 ;;; packages.el ends here
