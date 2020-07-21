6ad2bb3edfe81f87571e26aec57e940e5b1ad4e4
Andrew Tropin <andrew@trop.in>
2020-07-21 14:16:18 +0300

    Add hugo to org layer

--- dotfiles/.doom.d/init.el
+++ dotfiles/.doom.d/init.el
@@ -141,7 +141,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +present)               ; organize your plain life in plain text
+       (org +roam +present +hugo)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
