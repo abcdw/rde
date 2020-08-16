ebd90915ed0e281f1df52c4d9192c0d9f774ed02
Andrew Tropin <andrew@trop.in>
2020-08-16 10:32:43 +0300

    Add shellHook to shell.nix and todo

--- shell.nix
+++ shell.nix
@@ -3,4 +3,11 @@
 with pkgs;
 mkShell {
   buildInputs = [ gnumake stow nix-deploy nixFlakes];
+  # TODO: Add some proper doc to shellHook or similiar mechanism
+  # To make it easily explorable
+  shellHook = ''
+    echo -e "Some super cool docs about current project!!!!!!!!!!!!!!!!!"
+    echo -e "Some help about available aliases!!!!!!!!!!!!!!!!!"
+    echo -e "and make targets!!!!!!!!!!!!!!!!!"
+  '';
 }
