69502aeb53dc525a06e1705139d58d403f087efb
Andrew Tropin <andrew@trop.in>
2020-08-24 13:32:32 +0300

    Update shell.nix greeting message and format

--- shell.nix
+++ shell.nix
@@ -1,13 +1,14 @@
-{ pkgs ? import <nixpkgs> {} , ...}:
+{ pkgs ? import <nixpkgs> { }, ... }:
 
 with pkgs;
 mkShell {
-  buildInputs = [ gnumake stow nix-deploy nixFlakes];
+  buildInputs = [ gnumake stow nix-deploy nixFlakes ];
   # TODO: Add some proper doc to shellHook or similiar mechanism
   # To make it easily explorable
   shellHook = ''
-    echo -e "Some super cool docs about current project!!!!!!!!!!!!!!!!!"
-    echo -e "Some help about available aliases!!!!!!!!!!!!!!!!!"
-    echo -e "and make targets!!!!!!!!!!!!!!!!!"
+    echo
+    echo -e "To activate new configuration:"
+    echo -e "> make switch"
+    echo
   '';
 }
