1e6454c34594a94bda457ab36ad934109e944e14
Andrew Tropin <andrew@trop.in>
2020-08-24 12:10:05 +0300

    Create layout.nix, nix.nix and default.nix
    Contains shared code between hosts

---
+++ src/default.nix
@@ -0,0 +1,5 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  imports = [ ./nix.nix ./layout.nix ];
+}
---
+++ src/nix.nix
@@ -0,0 +1,14 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.unstable.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    registry.rde.flake = inputs.self;
+    registry.nixpkgs.flake = inputs.stable;
+    registry.stable.flake = inputs.stable;
+    registry.unstable.flake = inputs.unstable;
+  };
+}
