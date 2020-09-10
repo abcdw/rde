e89370506e6ba099891ba4b27fc654185c3b25c3
Andrew Tropin <andrew@trop.in>
2020-09-10 13:35:43 +0300

    Add working python.poetry template with README

--- templates/python/poetry/flake.nix
+++ templates/python/poetry/flake.nix
@@ -1,5 +1,31 @@
-{ config, lib, pkgs, ... }:
-
 {
-
+  description = "Description of your project goes here.";
+  inputs = {
+    stable.url = "github:NixOS/nixpkgs/nixos-20.03";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = inputs:
+    let
+      lib = inputs.stable.lib;
+      system = "x86_64-linux";
+      unstable-pkgs = inputs.unstable.legacyPackages.${system};
+      pkgs = inputs.stable.legacyPackages.${system};
+      runtimeDeps = with pkgs; [ ]; # Any system packages goes here
+      config = { projectDir = ./.; };
+      app = unstable-pkgs.poetry2nix.mkPoetryApplication config // {
+        propagatedBuildInputs = runtimeDeps;
+      };
+      # env = app.dependencyEnv;
+      env = pkgs.poetry2nix.mkPoetryEnv config;
+    in {
+      devShell."${system}" = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [ poetry ] ++ runtimeDeps
+          ++ (if (builtins.pathExists ./poetry.lock) then [ env ] else [ ]);
+        shellHook = ''
+          cat README
+        '';
+      };
+      defaultPackage."${system}" = app;
+    };
 }
