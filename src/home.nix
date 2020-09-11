c6988b5c0c39ebb6a0b2e0e7d37434710243a098
Andrew Tropin <andrew@trop.in>
2020-09-11 13:50:53 +0300

    Increase gpg agent cache ttl

--- src/home.nix
+++ src/home.nix
@@ -303,10 +303,10 @@
     services.gpg-agent = {
       enable = true;
       enableSshSupport = true;
-      defaultCacheTtl = 34560;
-      defaultCacheTtlSsh = 34560;
-      maxCacheTtl = 34560;
-      maxCacheTtlSsh = 34560;
+      defaultCacheTtl = 34560000;
+      defaultCacheTtlSsh = 34560000;
+      maxCacheTtl = 34560000;
+      maxCacheTtlSsh = 34560000;
     };
   };
 
