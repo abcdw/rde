d44f7460e6094f8d3157855bbcc1099e3e806e42
Andrew Tropin <andrew@trop.in>
2020-09-14 15:01:37 +0300

    Format docstrings

--- src/modules/default.nix
+++ src/modules/default.nix
@@ -10,18 +10,16 @@ with lib; {
       name = mkOption {
         type = types.str;
         description = ''
-          Person full name, will be used across
-             configs '';
+          Person full name, will be used across configs'';
       };
       username = mkOption {
         type = types.str;
-        description = "Username, will be used across configs ";
+        description = "Username, will be used across configs";
       };
       email = mkOption {
         type = types.str;
         description = ''
-          Email address, will be used
-             across configs '';
+          Email address, will be used across configs'';
       };
       fontSize = mkOption {
         default = 12;
@@ -33,7 +31,7 @@ with lib; {
         default = { };
         description = ''
           It's a proxy object, which just copies everything to
-                    home-manager.users.${config.rde.username}'';
+          home-manager.users.${config.rde.username}'';
       };
 
       zsh = { enable = mkEnableOption "zsh simple configuration"; };
