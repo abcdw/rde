61a19893053c09ae8beed97cc1d28db984527e6e
Andrew Tropin <andrew@trop.in>
2020-09-24 16:32:54 +0300

    Move most of the content of rde-configs to rde-defaults

--- src/modules/emacs/configs/rde-defaults/config.el
+++ src/modules/emacs/configs/rde-defaults/config.el
@@ -40,10 +40,21 @@
   (setq dired-listing-switches
         "-aFhl --group-directories-first --time-style=long-iso")
   (setq dired-hide-details-hide-symlink-targets nil)
-  :hook (dired-mode-hook . dired-hide-details-mode))
+  ;; Suggest other dired buffer path instead of current. Works
+  ;; similiar to midnight commander, when two dired buffers available
+  (setq dired-dwim-target t)
+  (defun rde/get-recentf-dired-list ()
+    (interactive)
+    (dired (cons "recentf" recentf-list)))
+  :hook (dired-mode-hook . dired-hide-details-mode)
+  :bind ("C-c f r" . rde/get-recentf-dired-list))
 
 (use-package dired-x
   :bind ("s-d" . dired-jump))
+
+(use-package project
+  :config
+  (define-key ctl-x-map "p" project-prefix-map))
 
 (use-package uniquify
   :config
@@ -72,6 +83,9 @@
 
 (use-package emacs
   :config
+  ;; Disabled by default, but pretty useful in some cases
+  (put 'narrow-to-region 'disabled nil)
+
   (defun rde/display-load-time ()
     (interactive)
     (message "rde emacs loaded in %s, C-h r i for search in emacs manual by topic. C-h C-a for welcome screen." (emacs-init-time)))
@@ -113,3 +127,93 @@
   (eshell-mode-hook .
    (lambda ()
      (define-key eshell-mode-map (kbd "s-e") 'switch-to-prev-buffer))))
+
+
+
+
+(defun run-command-in-eshell (cmd)
+  (eshell)
+  (eshell-kill-input)
+  (end-of-buffer)
+  (insert cmd)
+  (eshell-send-input))
+
+(defun rde/build ()
+  (interactive)
+  (run-command-in-eshell "nixos-rebuild build --flake /home/abcdw/work/rde"))
+
+(defun rde/switch ()
+  (interactive)
+  (run-command-in-eshell "sudo nixos-rebuild switch --flake /home/abcdw/work/rde"))
+
+(defun rde/switch-and-restart-emacs ()
+  (interactive)
+  (run-command-in-eshell "sudo nixos-rebuild switch --flake /home/abcdw/work/rde && restart-emacs"))
+
+;;; Configs to reference
+;;; https://github.com/bbatsov/emacs.d/blob/master/init.el
+
+;; It works
+
+
+;;; https://www.freedesktop.org/software/fontconfig/fontconfig-user.html
+
+;; ;; It doesn't
+
+
+(global-set-key (kbd "C-c r r") 'rde/switch-and-restart-emacs)
+(global-set-key (kbd "C-c f c") '(lambda () (interactive) (find-file "~/.config/emacs/init.el")))
+(global-set-key (kbd "C-c f d") '(lambda () (interactive) (find-file "~/work/rde/src/modules/emacs/default.nix")))
+(global-set-key (kbd "C-c f e") '(lambda () (interactive) (find-file "~/work/rde/src/modules/emacs/configs/rde-defaults/config.el")))
+
+(global-set-key (kbd "C-c f h") '(lambda () (interactive) (find-file "~/work/rde/src/home.nix")))
+(global-set-key (kbd "C-c f i") '(lambda () (interactive) (find-file "~/work/rde/src/hosts/ixy/configuration.nix")))
+(global-set-key (kbd "s-j") 'join-line)
+(global-set-key (kbd "s-o") 'other-window)
+(global-set-key (kbd "s-n") 'switch-to-next-buffer)
+(global-set-key (kbd "s-p") 'switch-to-prev-buffer)
+
+
+
+(use-package nix-mode
+  :defer t
+  :mode "\\.nix\\'"
+  :bind (:map nix-mode-map
+	      (("C-c l p" . nix-format-buffer))))
+
+(use-package magit
+  :defer t
+  :bind (("C-x g" . magit-status)))
+
+(use-package modus-operandi-theme
+  ;;; Read more here: https://protesilaos.com/modus-themes/
+  :config
+  (load-theme 'modus-operandi t))
+
+
+(use-package org :defer t)
+(use-package org-tempo :after org)
+;; (setq org-hide-emphasis-markers t)
+
+;; (use-package company-org-roam
+;;   :after org-roam company ; saves 0.3s startup time
+;;   :config
+;;   (push 'company-org-roam company-backends))
+
+(use-package company
+  :hook
+  (after-init-hook . global-company-mode))
+
+(use-package olivetti
+  :config
+  (setq olivetti-minimum-body-width 80)
+  :bind ("C-c t o" . olivetti-mode))
+
+(use-package restart-emacs
+  :commands restart-emacs
+  :bind ("C-c r e" . restart-emacs))
+
+(use-package keycast
+  ;; :config
+  ;; (setq keycast-window-predicate 'keycast-bottom-left-window-p)
+  :bind ("C-c t k" . keycast-mode))
