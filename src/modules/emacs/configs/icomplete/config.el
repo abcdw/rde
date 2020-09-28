17389317a844356c1c72f79864289f60a369a5bf
Andrew Tropin <andrew@trop.in>
2020-09-28 23:07:03 +0300

    Add eol or force-complete on C-e in icomplete

--- src/modules/emacs/configs/icomplete/config.el
+++ src/modules/emacs/configs/icomplete/config.el
@@ -5,12 +5,19 @@
     :config
     (setq completion-styles '(orderless)))
   (icomplete-mode 1)
+
+  (defun rde/icomplete-eol-or-complete (arg)
+    (interactive "^p")
+    (let ((original-point (point)))
+	  (move-end-of-line arg)
+	  (when (= original-point (point))
+	    (icomplete-force-complete))))
+
   ;; (setq icomplete-prospects-height 1)
   :bind (:map icomplete-minibuffer-map
               ;; ("<down>" . icomplete-forward-completions)
               ;; ("<up>" . icomplete-backward-completions)
-
-	      ("C-m" . icomplete-force-complete-and-exit)
+	      ("C-e" . rde/icomplete-eol-or-complete)
               ("C-n" . icomplete-forward-completions)
               ("C-p" . icomplete-backward-completions)))
 
