d8eae7836485854889b22deef1da6b82eb6875b4
Andrew Tropin <andrew@trop.in>
2020-10-01 16:45:40 +0300

    Add improvements for org mode and minibuffer

--- src/modules/emacs/configs/modus-themes/config.el
+++ src/modules/emacs/configs/modus-themes/config.el
@@ -1,4 +1,10 @@
+;;; Read more here: https://protesilaos.com/modus-themes/
 (use-package modus-operandi-theme
-  ;;; Read more here: https://protesilaos.com/modus-themes/
   :config
+  (setq modus-operandi-theme-prompts 'intense)
+  (setq modus-operandi-theme-completions 'opinionated)
+  (setq modus-operandi-theme-org-blocks 'rainbow)
+  (setq modus-operandi-theme-scale-headings t)
+  (setq modus-operandi-theme-headings
+      '((t . highlight)))
   (load-theme 'modus-operandi t))
