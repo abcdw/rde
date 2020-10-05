2b299f078de2034539bd09cfe5ee14748caa5c3b
Andrew Tropin <andrew@trop.in>
2020-10-05 16:22:30 +0300

    Add fixed-pitch for all links

--- src/modules/emacs/configs/faces/config.el
+++ src/modules/emacs/configs/faces/config.el
@@ -11,4 +11,6 @@
   (add-to-list 'default-frame-alist `(font . ,default-font))
   ;; (set-face-attribute 'variable-pitch nil :font "Source Sans Pro-10")
   (set-face-attribute 'variable-pitch nil :font "Open Sans-10")
+  ;; Without it tables becomes missaligned
+  (set-face-attribute 'link nil :inherit '(fixed-pitch button))
   ))
--- src/modules/emacs/configs/org/config.el
+++ src/modules/emacs/configs/org/config.el
@@ -5,8 +5,6 @@
   (setq org-hide-emphasis-markers t)
 
   (setq org-edit-src-content-indentation 0)
-  ;; Without it tables becomes missaligned
-  (set-face-attribute 'org-link nil :inherit '(fixed-pitch link))
   ;; To make width of checked checkbox the same as unchecked
   (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
   ;; (set-face-attribute 'org-table nil :inherit '(fixed-pitch))
