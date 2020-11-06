f3ecdc33c5f797ec635be1691946833fe732c09d
Andrew Tropin <andrew@trop.in>
2020-11-06 12:10:06 +0300

    Make links fixed-width

--- src/modules/emacs/configs/modus-themes/config.el
+++ src/modules/emacs/configs/modus-themes/config.el
@@ -6,4 +6,6 @@
   (setq modus-operandi-theme-org-blocks 'greyscale)
   (setq modus-operandi-theme-scale-headings t)
 
-  (load-theme 'modus-operandi t))
+  (load-theme 'modus-operandi t)
+  ;; Without it tables becomes missaligned
+  (set-face-attribute 'button nil :inherit '(fixed-pitch)))
