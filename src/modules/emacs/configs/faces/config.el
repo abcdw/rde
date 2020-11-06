95d4a9e47d2b81438881c877bb16788df9b90059
Andrew Tropin <andrew@trop.in>
2020-11-06 12:11:39 +0300

    Cleanup faces

--- src/modules/emacs/configs/faces/config.el
+++ src/modules/emacs/configs/faces/config.el
@@ -7,10 +7,8 @@
   (let* ((font-size (number-to-string rde/font-size))
 	 (default-font (string-join (list rde/font-family font-size) " ")))
 
-  (set-face-attribute 'default nil :font default-font)
-  (add-to-list 'default-frame-alist `(font . ,default-font))
+    (set-face-attribute 'default nil :font default-font)
+    (add-to-list 'default-frame-alist `(font . ,default-font)))
+
   ;; (set-face-attribute 'variable-pitch nil :font "Source Sans Pro-10")
-  (set-face-attribute 'variable-pitch nil :font "Open Sans-10")
-  ;; Without it tables becomes missaligned
-  (set-face-attribute 'link nil :inherit '(fixed-pitch button))
-  ))
+  (set-face-attribute 'variable-pitch nil :font "Open Sans-10"))
