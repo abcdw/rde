1f020ed58f53e6b927444543906efbcda3786839
Andrew Tropin <andrew@trop.in>
2020-11-29 14:12:28 +0300

    Use rde/font-size for varialbe pitch

--- src/modules/emacs/configs/faces/config.el
+++ src/modules/emacs/configs/faces/config.el
@@ -5,10 +5,12 @@
   :demand
   :config
   (let* ((font-size (number-to-string rde/font-size))
-	 (default-font (string-join (list rde/font-family font-size) " ")))
+	 (default-font (string-join (list rde/font-family font-size) " "))
+	 (sans-font (string-join (list "Open Sans" font-size) " ")))
 
     (set-face-attribute 'default nil :font default-font)
-    (add-to-list 'default-frame-alist `(font . ,default-font)))
+    (add-to-list 'default-frame-alist `(font . ,default-font))
+    (set-face-attribute 'variable-pitch nil :font sans-font))
 
   ;; (set-face-attribute 'variable-pitch nil :font "Source Sans Pro-10")
-  (set-face-attribute 'variable-pitch nil :font "Open Sans-10"))
+)
