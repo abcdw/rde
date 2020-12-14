5adb04bdb9323ca13623e179bacac8ee18986209
Andrew Tropin <andrew@trop.in>
2020-12-14 09:40:31 +0300

    Update emacs packages

--- rde/emacs/package.scm
+++ rde/emacs/package.scm
@@ -16,63 +16,16 @@
 (define-public emacs-rde
   (package
     (name "emacs-rde")
-    (version "0.1.0")
+    (version "0.2.0")
     (build-system emacs-build-system)
     (source (local-file "emacs-rde.el"))
+    (propagated-inputs `(("which-key" ,emacs-which-key)
+			 ("use-package" ,emacs-use-package)))
     (synopsis "test")
     (description "test")
     (home-page "https://github.com/abcdw/rde")
     (license license:gpl3+)))
 
-
-(define-public emacs-rde-use-package
-  (package/inherit emacs-use-package
-    (name "emacs-next-use-package")
-    (version "2.4.1")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/jwiegley/use-package")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "088kl3bml0rs5bkfymgzr15ram9qvy66h1kaisrbkynh0yxvf8g9"))))
-    (propagated-inputs '())))
-
-(define-public emacs-next-pgtk
-  (let ((commit "3df4ca451d41a5f1036713277ef55ca9734c6fa7")
-        (revision "0"))
-    (package/inherit emacs-next
-      (name "emacs-next-pgtk")
-      (version (git-version "28.0.50" revision commit))
-      (source
-       (origin
-	 (method git-fetch)
-	 (uri (git-reference
-	       (url "https://github.com/masm11/emacs")
-	       (commit commit)))
-	 (sha256
-          (base32
-           "1c677c97b9avhlzysg09cvf6bd69iz41ggppnszw8chhphk3knfc"))))
-      (arguments
-       (substitute-keyword-arguments
-	   (package-arguments emacs-next)
-	 ((#:configure-flags flags ''())
-	  `(cons* "--with-pgtk" "--with-xwidgets" ,flags))))
-      (propagated-inputs `(("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
-			   ("glib-networking" ,glib-networking)))
-      (inputs
-       `(("webkitgtk" ,webkitgtk)
-	 ,@(package-inputs emacs-next)))
-
-      (home-page "https://github.com/masm11/emacs")
-      (synopsis "The fork of extensible, customizable,
-self-documenting text editor (with pgtk toolkit and xwidgets
-support)")
-      (description "This is an unofficial Emacs fork build with
-pure-gtk to work natively on Wayland. In addition to that xwidgets
-also enabled and work without glitches even on X server."))))
 
 ;; (symlink "./emacs-rde.el" "/home/abcdw/tmp-file.el")
 
@@ -102,11 +55,14 @@ also enabled and work without glitches even on X server."))))
  ;; (append (map rde-emacs-instead-of-emacs rde-emacs-packages)
  ;; 	 '(rde-emacs))
  (list
-  emacs-next-pgtk
+  emacs
+  ;; emacs-next-pgtk
   ;; (rde-emacs-instead-of-emacs
   ;;  )
-  emacs-rde-use-package
+  emacs-use-package
+  emacs-which-key
   emacs-magit
+  emacs-rde
   ;; emacs-ivy
   ))
 
