94b1cb495d9b3a5a49b096257abd25204ac9d024
Andrew Tropin <andrew@trop.in>
2020-12-15 14:52:55 +0100

    Update scripts/install.sh

--- rde/scripts/install.sh
+++ rde/scripts/install.sh
@@ -1,17 +1,42 @@
 mkdir ~/work
 cd ~/work
+
 git clone https://github.com/abcdw/rde
 
 git config --global user.email "andrew@trop.in"
 git config --global user.name "Andrew Tropin"
 
+ln -sf /home/guest/work/rde/rde/sway ~/.config/
+ln -sf /home/guest/work/rde/stale/dotfiles/.config/alacritty ~/.config/
+ln -sf /home/guest/work/rde/stale/dotfiles/.tmux.conf ~/
+
 sudo -s
+cryptsetup luksOpen /dev/nvme0n1p5 enc
+mount -t btrfs /dev/mapper/enc /mnt
+
+
+
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/boot
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/gnu
+btrfs subvolume create /mnt/data
+btrfs subvolume create /mnt/log
+
+btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+
+umount /mnt
+
+
+mount -o subvol=root /dev/mapper/enc /mnt
+
 cd /mnt
 mkdir home
 mkdir gnu
 mkdir data
 mkdir var/log -p
 mkdir boot
+
 
 mount -o subvol=home /dev/mapper/enc home
 mount -o subvol=gnu /dev/mapper/enc gnu
