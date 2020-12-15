d0e5428ace863711f5e282354eb101136bf32bca
Andrew Tropin <andrew@trop.in>
2020-12-15 13:28:58 +0300

    Cleanup install.scm

--- rde/system/install.scm
+++ rde/system/install.scm
@@ -7,6 +7,7 @@
   #:use-module (gnu packages bash)
   #:use-module (gnu packages admin)
   #:use-module (gnu packages fonts)
+  #:use-module (gnu packages linux)
   #:use-module (gnu system keyboard)
   #:use-module ((gnu system install) #:prefix gnu-system-install:)
   #:use-module (gnu system file-systems)
@@ -15,9 +16,9 @@
   #:use-module (gnu system pam)
   #:use-module (gnu services)
   #:use-module (gnu services base)
-  #:use-module (gnu services xorg)
   #:use-module (gnu services desktop)
   #:use-module (gnu services sddm)
+  #:use-module (gnu services dbus)
   #:use-module (gnu services security-token)
   #:use-module (srfi srfi-1)
   #:use-module (ice-9 pretty-print)
@@ -25,9 +26,12 @@
   #:use-module (rde emacs packages)
   #:export (installation-os))
 
+
 (define installation-os
   (operating-system
     (inherit gnu-system-install:installation-os)
+
+    ;; TODO: Move layout to flavoured config?
     (keyboard-layout
      (keyboard-layout "us,ru" "dvorak,"
 		      #:options '("grp:win_space_toggle" "ctrl:nocaps")))
@@ -47,40 +51,18 @@
 	       %base-packages-disk-utilities
 	       %base-packages))
 
-    (services (append (list (service pcscd-service-type))
-		      (operating-system-services gnu-system-install:installation-os)))
-    ;; (services
-    ;;  (append
-    ;;   (list
-    ;;    ;; (service documentation-service-type "tty2")
-    ;;    (service pcscd-service-type)
-    ;;    ;; (service console-font-service-type
-    ;;    ;;          (map (match-lambda
-    ;;    ;;                 ("tty2"
-    ;;    ;;                  '("tty2" . "LatGrkCyr-8x16"))
-    ;;    ;; 		       ("tty3"
-    ;;    ;; 			`("tty3" . ,(file-append
-    ;;    ;; 				     font-terminus
-    ;;    ;; 				     "/share/consolefonts/ter-132n")))
-    ;;    ;;                 (tty
-    ;;    ;;                  ;; Use a font that doesn't have more than 256
-    ;;    ;;                  ;; glyphs so that we can use colors with varying
-    ;;    ;;                  ;; brightness levels (see note in setfont(8)).
-    ;;    ;;                  `(,tty . "lat9u-16")))
-    ;;    ;;               '("tty1" "tty2" "tty3" "tty4" "tty5" "tty6")))
-    ;;    )
-    ;;   (operating-system-services gnu-system-install:installation-os)
-    ;;   ;; (cons*
-    ;;   ;;  (modify-services
-    ;;   ;; 	(remove (lambda (service)
-    ;;   ;;             (member (service-kind service)
-    ;;   ;;                     (list
-    ;;   ;;                      gdm-service-type
-    ;;   ;; 			   console-font-service-type
-    ;;   ;;                      )))
-    ;;   ;; 		)))
-    ;;   ))
-    ))
+    (services
+     (append
+      (list (service pcscd-service-type)
+	    ;; (dbus-service)
+	    (polkit-service)
+	    (service mingetty-service-type (mingetty-configuration
+                                            (tty "tty8")))
+	    (elogind-service))
+      (operating-system-user-services gnu-system-install:installation-os)))
+
+    (setuid-programs (list (file-append shadow "/bin/passwd")
+			   (file-append sudo "/bin/sudo")))))
 
 (pretty-print (map service-kind (operating-system-services installation-os)))
 installation-os
