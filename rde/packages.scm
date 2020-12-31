b2742b5d2483d1dfe77a761a03927a73f66926c7
Andrew Tropin <andrew@trop.in>
2020-12-31 13:12:05 +0300

    Update rde/packages

--- rde/packages.scm
+++ rde/packages.scm
@@ -1,13 +1,19 @@
 (define-module (rde packages)
   #:use-module (gnu packages)
-  #:use-module (rde emacs packages))
+  #:use-module (rde emacs packages)
+  #:use-module (rde gnupg packages))
 
+(use-modules
+ (gnu packages)
+ (rde emacs packages)
+ (rde gnupg packages))
 
 (define-public %rde-base-packages
   (map specification->package+output
-       '("tmux")))
+       '("tmux" "openssh" "git" "make")))
 
 (define-public %rde-all-packages
   (append
    %rde-base-packages
+   %rde-gnupg-packages
    %rde-emacs-all-packages))
