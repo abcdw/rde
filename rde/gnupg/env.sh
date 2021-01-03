f644ce4e801c3583de9c0d795983f948b01e6aa9
Andrew Tropin <andrew@trop.in>
2021-01-03 11:02:30 +0300

    Add sway on login start on first tty

--- rde/gnupg/env.sh
+++ rde/gnupg/env.sh
@@ -2,3 +2,9 @@ export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 # export GPG_TTY=$(tty)
 
 gpgconf --launch gpg-agent
+
+light -S 100
+
+if [ "$(tty)" = "/dev/tty1" ]; then
+	exec sway
+fi
