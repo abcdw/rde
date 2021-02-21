7165853009b44f7c3c69422fc9a3dd61d85639e5
Andrew Tropin <andrew@trop.in>
2021-02-21 12:02:01 +0300

    gnu: home-services: gnupg: Add home-gnupg-configuration

--- gnu/home-services/gnupg.scm
+++ gnu/home-services/gnupg.scm
@@ -3,20 +3,38 @@
   #:use-module (gnu home-services shepherd)
   #:use-module (gnu packages gnupg)
   #:use-module (guix gexp)
+  #:use-module (guix records)
 
-  #:export (home-gnupg-service-type))
+  #:export (home-gnupg-service-type
+	    home-gnupg-configuration))
 
-(define (add-ssh-agent-socket %)
+(define-record-type* <home-gnupg-configuration>
+  home-gnupg-configuration make-home-gnupg-configuration
+  home-gnupg-configuration?
+  (package     home-gnupg-configuration-package
+               (default gnupg))
+  (ssh-agent   home-gnupg-configuration-ssh-agent
+               (default #f)))
+
+(define (add-ssh-agent-socket config)
   "Adds SSH_AUTH_SOCK variable to user's environment."
-  `(("SSH_AUTH_SOCK" "$(" ,(file-append gnupg "/bin/gpgconf") " --list-dirs agent-ssh-socket)")))
+  (if (home-gnupg-configuration-ssh-agent config)
+      `(("SSH_AUTH_SOCK"
+	 "$("
+	 ,(file-append gnupg "/bin/gpgconf")
+	 " --list-dirs agent-ssh-socket)"))
+      '()))
 
-(define (add-gnupg-agent-to-services %)
-  (list
-   (shepherd-service
-    (documentation "Run and control gpg-agent.")
-    (provision '(gpg-agent ssh-agent))
-    (start #~(make-system-constructor "gpg-connect-agent /bye"))
-    (stop #~(make-system-destructor "gpgconf --kill gpg-agent")))))
+(define (add-home-gnupg-agent-to-shepherd config)
+  (let ((provision-list `(gpg-agent
+			  ,@(if (home-gnupg-configuration-ssh-agent config)
+				'(ssh-agent) '()))))
+    (list
+     (shepherd-service
+      (documentation "Run and control gpg-agent.")
+      (provision provision-list)
+      (start #~(make-system-constructor "gpg-connect-agent /bye"))
+      (stop #~(make-system-destructor "gpgconf --kill gpg-agent"))))))
 
 (define home-gnupg-service-type
   (service-type (name 'home-gnupg)
@@ -26,9 +44,10 @@
                         add-ssh-agent-socket)
 		       (service-extension
 			home-shepherd-service-type
-			add-gnupg-agent-to-services)
+			add-home-gnupg-agent-to-shepherd)
 		       (service-extension
 			home-profile-service-type
-			(lambda (%) `(,gnupg)))))
-		(default-value #f)
-                (description "Configures and installs gpg-agent.")))
+			(lambda (config)
+			  (list (home-gnupg-configuration-package config))))))
+		(default-value (home-gnupg-configuration))
+                (description "Configure and install gpg-agent.")))
