efdd1c31d29cb472f8fa25e499050cf80a81b3e0
Andrew Tropin <andrew@trop.in>
2021-04-02 13:44:06 +0300

    gnu: home-services: base: Add extensions argument to generic-service

--- gnu/home-services/base.scm
+++ gnu/home-services/base.scm
@@ -2,22 +2,39 @@
   #:use-module (gnu home-services)
   #:use-module (gnu home-services files)
   #:use-module (gnu services)
+  #:use-module (ice-9 match)
+
   #:export (home-generic-service)
   #:re-export (simple-service))
 
 
-(define* (home-generic-service name #:key (files '()) (packages '()))
+(define* (home-generic-service
+	  name
+	  #:key
+	  (files '())
+	  (packages '())
+	  (extensions '()))
   "Creates a service which extends home-profile with PACKAGES and
-home-files with FILES."
+home-files with FILES.  EXTENSIONS is an alist of pairs @code{(target
+. function)}."
   (let* ((profile-extension (service-extension
 			     home-profile-service-type
 			     (const packages)))
 	 (files-extension   (service-extension
 			     home-files-service-type
 			     (const files)))
-         (type              (service-type
+	 (more-extensions   (map
+			     (match-lambda
+			       ((target . value)
+				(service-extension
+				 target
+				 (const value))))
+			     extensions))
+	 (type              (service-type
 			     (name name)
 			     (extensions
-			      (list profile-extension
-				    files-extension)))))
+			      (append
+			       more-extensions
+			       (list profile-extension
+				     files-extension))))))
     (service type #f)))
