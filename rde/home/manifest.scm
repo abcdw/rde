4c5109783725ec2d0e71f652020a4004b76c3193
Andrew Tropin <andrew@trop.in>
2021-04-14 08:35:34 +0300

    rde: packages: Add xdg-desktop-portal-wlr draft, cleanup rde

--- rde/home/manifest.scm
+++
@@ -1,6 +0,0 @@
-(define-module (rde home manifest)
-  #:use-module (rde packages)
-  #:use-module (guix profiles))
-
-(packages->manifest
- %rde-all-packages)
--- rde/packages.scm
+++ rde/packages.scm
@@ -1,13 +1,41 @@
 (define-module (rde packages)
   #:use-module (gnu packages)
-  #:use-module (rde emacs packages)
-  #:use-module (rde gnupg packages)
-  #:use-module (rde obs))
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xdisorg)
 
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system meson)
+  #:use-module ((guix licenses) #:prefix license:))
 
-(define-public %rde-base-packages
-  (map specification->package+output
-       '()))
-
-(define-public %rde-all-packages
-  (list))
+(define-public xdg-desktop-portal-wlr
+  (package
+    (name "xdg-desktop-portal-wlr")
+    (version "0.2.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/emersion/xdg-desktop-portal-wlr")
+                    (commit (string-append "v" version))))
+              (sha256
+               (base32 "1vjz0y3ib1xw25z8hl679l2p6g4zcg7b8fcd502bhmnqgwgdcsfx"))))
+    (build-system meson-build-system)
+    (native-inputs
+     `(("wayland-protocols" ,wayland-protocols)
+       ("pkg-config" ,pkg-config)))
+    (inputs
+     `(("pipewire" ,pipewire-0.3)
+       ("wayland" ,wayland)
+       ("elogind" ,elogind)
+       ("libdrm" ,libdrm)))
+    (arguments
+     `(#:configure-flags
+       '("-Dsystemd=disabled"
+         "-Dsd-bus-provider=libelogind")))
+    (synopsis "")
+    (description "")
+    (home-page "")
+    (license license:expat)))
