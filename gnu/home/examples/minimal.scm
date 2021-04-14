95888c35c1ee17071e86da256ff6967803284aee
Andrew Tropin <andrew@trop.in>
2021-04-14 18:56:22 +0300

    gnu: home: examples: Add minimal.scm

---
+++ gnu/home/examples/minimal.scm
@@ -0,0 +1,38 @@
+;; -*- mode: scheme -*-
+(use-modules (gnu home)
+	     (gnu home-services)
+	     (gnu home-services gnupg)
+	     (gnu home-services ssh)
+	     (gnu home-services version-control)
+	     (gnu home-services files)
+	     (gnu home-services state)
+	     (gnu home-services mcron)
+	     (gnu services)
+	     (gnu packages)
+	     (gnu packages linux)
+	     (gnu packages admin)
+	     (gnu packages gnupg)
+	     (gnu system keyboard)
+	     (guix gexp))
+
+
+(define sample-he
+  (home-environment
+   (home-directory (getenv "HOME"))
+   ;; (symlink-name ".guix-home-env")
+   (packages (list htop))
+   (services
+    (list
+     (simple-service
+      'test-config home-files-service-type
+      (list `("config/test.conf"
+              ,(plain-file "tmp-file.txt" "hehe"))))
+
+     (service home-ssh-service-type
+	      (home-ssh-configuration
+	       (extra-config
+		(list
+		 (ssh-host "savannah"
+			   '((compression . #f)))))))))))
+
+sample-he
