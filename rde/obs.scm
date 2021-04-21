705ea16dbe630f0811c4ca1dac8625768394344b
Andrew Tropin <andrew@trop.in>
2021-04-21 15:58:02 +0300

    rde: obs: Comment out obs-next package
    guix weather doesn't work with it

--- rde/obs.scm
+++ rde/obs.scm
@@ -7,17 +7,17 @@
   #:use-module (guix gexp)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public obs-next
-  (package/inherit obs
-    (name "obs-next")
-    (version "master")
-    (source
-     (local-file "/home/bob/work/obs-studio" #:recursive? #t))
-    (native-search-paths
-     (list
-      (search-path-specification
-       (variable "OBS_PLUGINS_DATA_PATH")
-       (files '("share/obs/obs-plugins")))
-      (search-path-specification
-       (variable "OBS_PLUGINS_PATH")
-       (files '("lib/obs-plugins")))))))
+;; (define-public obs-next
+;;   (package/inherit obs
+;;     (name "obs-next")
+;;     (version "master")
+;;     (source
+;;      (local-file "/home/bob/work/obs-studio" #:recursive? #t))
+;;     (native-search-paths
+;;      (list
+;;       (search-path-specification
+;;        (variable "OBS_PLUGINS_DATA_PATH")
+;;        (files '("share/obs/obs-plugins")))
+;;       (search-path-specification
+;;        (variable "OBS_PLUGINS_PATH")
+;;        (files '("lib/obs-plugins")))))))
