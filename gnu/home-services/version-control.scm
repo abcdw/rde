19e8310fdd54e1d213ec1f3bbe240aadb8cbb688
Xinglu Chen <public@yoctocell.xyz>
2021-05-04 19:35:00 +0200

    gnu: home-services: version-control: Add Mercurial
    * gnu/home-services/version-control.scm (mercurial-config?,
    serialize-ini-config, serialize-mercurial-ignore, mercurial-ignore?):
    New dummy procedures.
    (home-mercurial-configuration): New record type.
    (serialize-mercurial-config, serialize-mercurial-ignores,
    home-mercurial-files-service, home-mercurial-profile-service): New procedures.
    (home-mercurial-service-type): New service-type.

--- gnu/home-services/version-control.scm
+++ gnu/home-services/version-control.scm
@@ -1,6 +1,8 @@
 (define-module (gnu home-services version-control)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-9)
+  #:use-module (srfi srfi-11)
+  #:use-module (srfi srfi-26)
   #:use-module (ice-9 match)
   #:use-module (gnu home-services)
   #:use-module (gnu home-services-utils)
@@ -10,11 +12,15 @@
   #:use-module (guix packages)
   #:use-module (guix gexp)
   #:use-module (guix records)
+  #:use-module ((guix import utils) #:select (flatten))
 
   #:export (home-git-configuration
 	    home-git-extension
 	    home-git-service-type
-	    serialize-git-config))
+	    serialize-git-config
+
+            home-mercurial-configuration
+            home-mercurial-service-type))
 
 ;;; Commentary:
 ;;;
@@ -221,4 +227,170 @@ of the configuration file."))
                 (default-value (home-git-configuration))
                 (description "Install and configure Git.")))
 
+
+;;;
+;;; Mercurial.
+;;;
+;;; (home-mercurial-configuration
+;;;   (regexp-ignore '("^\\.pc/"))
+;;;   (glob-ignore '("*.elc" "*~"))
+;;;   (config
+;;;    '((commands
+;;;       ((commit.post-status . #t)))
+;;;      (ui
+;;;       ((username . "Alice Bobson <charlie@example.org")))
+;;;      (defaults
+;;;        (log . "-v")))))
+;;;
 
+(define mercurial-config? list?)
+(define (serialize-ini-config field-name val) "")
+(define (serialize-mercurial-ignore field-name val) "")
+(define mercurial-ignore? list?)
+
+;; TODO: Add support for Mercurial (Python) extensions?
+;; TODO: Add separate field for name and email?
+(define-configuration home-mercurial-configuration
+  (package
+    (package mercurial)
+    "The Mercurial package to use.")
+  (regexp-ignore
+   (mercurial-ignore '())
+   "List of regular expressions to ignore globally.  The default syntax
+is Python/Perl-style regular expression (see @command{man 5 hgignore}).
+
+The @code{*-ignore} fields are equivalent to adding @code{ui.ignore =
+/file/with/ignore/rules} in your @file{hgrc}.")
+  (glob-ignore
+   (mercurial-ignore '())
+   "List of globs to ignore globally.")
+  (rootglob-ignore
+   (mercurial-ignore '())
+   "List of @dfn{rootglobs} to ignore globally.")
+  (config
+   (ini-config '())
+   "List of list representing the contents of the @file{hgrc}
+configuration file.  The syntax is similar to that of the Git service.
+The key of a pair can be a symbol or string, and the value can be a
+boolean, string, symbol, number, gexp (@pxref{gexp,,,guix.info}), or a
+list of one the above.
+
+@lisp
+(config
+ `((commands
+    ((commit.post-status . #t)))
+   (graph
+    ((width . 4)))
+   (hooks
+    ((incoming.email . ,(local-file \"/path/to/email/hook\"))))))
+@end lisp
+
+will turn into this:
+
+@example
+[commands]
+    commit.post-status = True
+[graph]
+    width = 4
+[hooks]
+  incoming.email = /gnu/store/123...-email-hook
+@end example"))
+
+(define (serialize-mercurial-config config)
+  (define (serialize-boolean val)
+    (list (if val "True" "False")))
+
+  (define (serialize-list val)
+    (interpose (map serialize-val val) ", "))
+
+  (define (serialize-val val)
+    (cond
+     ((list? val) (serialize-list val))
+     ((boolean? val) (serialize-boolean val))
+     ((or (number? val) (symbol? val)) (list (maybe-object->string val)))
+     (else (list val))))
+
+  (define (serialize-field key val)
+    (let ((val (serialize-val val))
+          (key (symbol->string key)))
+      `(,key " = " ,@val "\n")))
+
+  (flatten (generic-serialize-ini-config
+            #:combine-ini interpose
+            #:combine-alist list
+            #:combine-section-alist cons
+            #:serialize-field serialize-field
+            #:fields config)))
+
+(define* (serialize-mercurial-ignores #:key regexp glob rootglob)
+  (define (add-ignore lst type)
+    (if (not (null? lst))
+        (string-append (format #f "syntax: ~a\n" type)
+                       (string-join lst "\n" 'suffix))
+        ""))
+
+  (string-join (map (cut add-ignore <> <>)
+                    (list regexp glob rootglob)
+                         '(regexp glob rootglob))
+               "\n"))
+
+(define (home-mercurial-files-service config)
+  (define rest cdr)
+
+  (define (compare-sections section1 section2)
+    (string<? (symbol->string (first section1))
+              (symbol->string (first section2))))
+
+  (define (fold-sections section1 section2)
+    (cond
+     ((equal? (first section1) (first section2))
+      (list (list (first section1)
+                  (append (second section1) (second section2)))))
+     (else
+      (list section1 section2))))
+
+  (define (merge-sections config)
+    (let ((sorted-config (sort config compare-sections)))
+      (fold (lambda (section acc)
+              (if (null? acc)
+                  (list section)
+                  (append (fold-sections section (first acc))
+                          (rest acc))))
+            '()
+            sorted-config)))
+
+  (let* ((ignores (serialize-mercurial-ignores
+                   #:regexp
+                   (home-mercurial-configuration-regexp-ignore config)
+                   #:glob
+                   (home-mercurial-configuration-glob-ignore config)
+                   #:rootglob
+                   (home-mercurial-configuration-rootglob-ignore config)))
+         (final-config (merge-sections
+                        (append (home-mercurial-configuration-config config)
+                                `((ui
+                                   ((ignore . ,(plain-file "hg-ignores"
+                                                           ignores)))))))))
+    `(("config/hg/hgrc"
+       ,(apply mixed-text-file
+               "hgrc"
+               (serialize-mercurial-config final-config))))))
+
+(define (home-mercurial-profile-service config)
+  (list (home-mercurial-configuration-package config)))
+
+(define home-mercurial-service-type
+  (service-type (name 'home-mercurial)
+                (extensions
+                 (list (service-extension
+                        home-files-service-type
+                        home-mercurial-files-service)
+                       (service-extension
+                        home-profile-service-type
+                        home-mercurial-profile-service)))
+                ;; TODO: Add extension mechanism
+                ;; (compose identity)
+                ;; (extend home-mercurial-extensions)
+                (default-value (home-mercurial-configuration))
+                (description "\
+Install and configure the Mercurial version control system.")))
