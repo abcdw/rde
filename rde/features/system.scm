0c6da19b0021dbafaeeda79d785fdf0478a45ea3
Andrew Tropin <andrew@trop.in>
2021-05-17 08:16:56 +0300

    rde: features: Add feature-bootloader and feature-file-systems

---
+++ rde/features/system.scm
@@ -0,0 +1,42 @@
+(define-module (rde features system)
+  #:use-module (rde features)
+  #:use-module (gnu bootloader)
+  #:use-module (gnu bootloader grub)
+  #:use-module (gnu system file-systems)
+  #:use-module (srfi srfi-1)
+
+  #:export (feature-bootloader
+	    feature-file-systems))
+
+(define %default-bootloader-configuration
+  (bootloader-configuration
+   (bootloader grub-efi-bootloader)
+   (target "/boot/efi")))
+
+(define* (feature-bootloader
+	  #:key (bootloader-configuration %default-bootloader-configuration))
+  "Provides custom bootloader configuration for operating-system.
+keyboard-layout will be overriden by feature-keyboard if it present."
+  (ensure-pred bootloader-configuration? bootloader-configuration)
+
+  (feature
+   (name 'bootloader)
+   (values (make-feature-values bootloader-configuration))))
+
+
+(define (list-of-file-systems? lst)
+  (and (list? lst) (every file-system? lst)))
+
+(define* (feature-file-systems
+	  #:key
+	  (file-systems '())
+	  (base-file-systems %base-file-systems))
+  "Provides file systems for operating-system.  By default
+%base-file-systems will be added to the end of FILE-SYSTEMS, this
+behavior can be overriden with BASE-FILE-SYSTEM argument."
+  (ensure-pred list-of-file-systems? file-systems)
+
+  (let ((file-systems (append file-systems base-file-systems)))
+    (feature
+     (name 'file-systems)
+     (values (make-feature-values file-systems)))))
