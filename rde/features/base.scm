edb5859c6878cc8d088f4f45adf3abf687e73ae5
Andrew Tropin <andrew@trop.in>
2021-05-18 11:08:32 +0300

    rde: features: base: Add feature-packages

--- rde/features/base.scm
+++ rde/features/base.scm
@@ -1,5 +1,11 @@
 (define-module (rde features base)
   #:use-module (rde features)
+  #:use-module (gnu system)
+  #:use-module (gnu services)
+  #:use-module (gnu home-services)
+  #:use-module (gnu packages certs)
+  #:use-module (guix packages)
+  #:use-module (srfi srfi-1)
 
   #:export (feature-user-info))
 
@@ -16,3 +22,40 @@
    (name 'user-info)
    (values (make-feature-values
 	    user-name full-name email home-directory))))
+
+
+(define (list-of-packages? lst)
+  (and (list? lst) (every package? lst)))
+
+;; TODO: Cleanup the list of base system packages, it contains some
+;; unecessary for rde packages
+(define %base-system-packages
+  (append
+   (list nss-certs)
+   %base-packages-disk-utilities
+   %base-packages))
+
+(define* (feature-packages
+	  #:key
+	  (home-packages '())
+	  (system-packages '())
+	  (base-system-packages %base-system-packages))
+  "Allows to specify standalone packages for home-environment, or
+operating-system, or both."
+  (ensure-pred list-of-packages? home-packages)
+  (ensure-pred list-of-packages? system-packages)
+
+  (define (get-home-packages values)
+    (list
+     (service home-profile-service-type home-packages)))
+
+  (define (get-system-packages values)
+    (list
+     (service profile-service-type
+	      (append system-packages
+		      base-system-packages))))
+
+  (feature
+   (name 'packages)
+   (home-services-getter get-home-packages)
+   (system-services-getter get-system-packages)))
