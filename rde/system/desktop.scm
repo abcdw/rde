ad1d6f201ff10fe415ca4e85d9155c0e374a65f0
Andrew Tropin <andrew@trop.in>
2021-05-18 13:44:55 +0300

    rde: system: Prettify file-systems

--- rde/system/desktop.scm
+++ rde/system/desktop.scm
@@ -40,121 +40,100 @@
 
 (define os
   (operating-system
-   (host-name "antelope")
-   (timezone "Europe/Moscow")
-   (locale "en_US.utf8")
+    (host-name "antelope")
+    (timezone "Europe/Moscow")
+    (locale "en_US.utf8")
 
-   ;; Choose US English keyboard layout.  The "altgr-intl"
-   ;; variant provides dead keys for accented characters.
+    (keyboard-layout
+     (keyboard-layout "us,ru" "dvorak,"
+		      #:options '("grp:win_space_toggle" "ctrl:nocaps")))
+    ;; Use the UEFI variant of GRUB with the EFI System
+    ;; Partition mounted on /boot/efi.
+    (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (target "/boot/efi")
+                 (keyboard-layout keyboard-layout)))
 
-   (keyboard-layout
-    (keyboard-layout "us,ru" "dvorak,"
-		     #:options '("grp:win_space_toggle" "ctrl:nocaps")))
-   ;; Use the UEFI variant of GRUB with the EFI System
-   ;; Partition mounted on /boot/efi.
-   (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (target "/boot/efi")
-                (keyboard-layout keyboard-layout)))
+    ;; Specify a mapped device for the encrypted root partition.
+    ;; The UUID is that returned by 'cryptsetup luksUUID'.
 
-   ;; Specify a mapped device for the encrypted root partition.
-   ;; The UUID is that returned by 'cryptsetup luksUUID'.
+    ;; grub 2.0.4 doesn't support luks2
+    ;; <https://wiki.archlinux.org/title/GRUB>
+    (mapped-devices (list (mapped-device
+                           (source (uuid "0e51ee1e-49ef-45c6-b0c3-6307e9980fa9"))
+                           (target "enc")
+                           (type luks-device-mapping))))
 
-   (mapped-devices (list (mapped-device
-                          (source (uuid "0e51ee1e-49ef-45c6-b0c3-6307e9980fa9"))
-                          (target "enc")
-                          (type luks-device-mapping))))
+    (file-systems
+     (append
+      (map (match-lambda
+	     ((subvol . mount-point)
+	      (file-system
+		(type "btrfs")
+		(device "/dev/mapper/enc")
+		(mount-point mount-point)
+		(options (format #f "subvol=~a" subvol))
+		(dependencies mapped-devices))))
+	   '((root . "/")
+	     (boot . "/boot")
+	     (gnu  . "/gnu")
+	     (home . "/home")
+	     (data . "/data")
+	     (log  . "/var/log")))
+      (list
+       (file-system
+	 (mount-point "/boot/efi")
+         (type "vfat")
+	 (device (uuid "8C99-0704" 'fat32))))
+      %base-file-systems))
 
-   (file-systems (cons* (file-system
-                         (device "/dev/mapper/enc")
-                         (mount-point "/")
-                         (type "btrfs")
-			 (options "subvol=root")
-                         ;; (flags '(no-atime))
-                         (dependencies mapped-devices))
-			(file-system
-                         (device "/dev/mapper/enc")
-                         (mount-point "/boot")
-                         (type "btrfs")
-			 (options "subvol=boot")
-                         (dependencies mapped-devices))
-			(file-system
-                         (device "/dev/mapper/enc")
-                         (mount-point "/gnu")
-                         (type "btrfs")
-			 (options "subvol=gnu")
-                         (dependencies mapped-devices))
-			(file-system
-                         (device "/dev/mapper/enc")
-                         (mount-point "/home")
-                         (type "btrfs")
-			 (options "subvol=home")
-                         (dependencies mapped-devices))
-			(file-system
-                         (device "/dev/mapper/enc")
-                         (mount-point "/var/log")
-                         (type "btrfs")
-			 (options "subvol=log")
-                         (dependencies mapped-devices))
-			(file-system
-                         (device "/dev/mapper/enc")
-                         (mount-point "/data")
-                         (type "btrfs")
-			 (options "subvol=data")
-                         (dependencies mapped-devices))
-			(file-system
-			 (mount-point "/boot/efi")
-                         (type "vfat")
-			 (device (uuid "8C99-0704" 'fat32)))
-			%base-file-systems))
+    ;; Create user `bob' with `alice' as its initial password.
+    (users (cons (user-account
+                  (name "bob")
+                  (comment "Alice's brother")
+                  (password (crypt "alice" "$6$abc"))
+                  (group "users")
+                  (supplementary-groups '("wheel" "netdev"
+                                          "audio" "video")))
+		 %base-user-accounts))
 
-   ;; Create user `bob' with `alice' as its initial password.
-   (users (cons (user-account
-                 (name "bob")
-                 (comment "Alice's brother")
-                 (password (crypt "alice" "$6$abc"))
-                 (group "users")
-                 (supplementary-groups '("wheel" "netdev"
-                                         "audio" "video")))
-		%base-user-accounts))
+    ;; This is where we specify system-wide packages.
+    (packages (append
+	       (map specification->package+output
+		    '(;; System packages
+		      "nss-certs"))
+	       %base-packages-disk-utilities
+	       %base-packages))
 
-   ;; This is where we specify system-wide packages.
-   (packages (append
-	      (map specification->package+output
-		   '(;; System packages
-		     "nss-certs"))
-	      %base-packages-disk-utilities
-	      %base-packages))
+    (kernel-loadable-modules (list v4l2loopback-linux-module))
+    (services
+     (append
+      (list
+       ;; (simple-service
+       ;;  'add-xdg-desktop-portals
+       ;;  dbus-root-service-type
+       ;;  (list xdg-desktop-portal xdg-desktop-portal-wlr))
+       (simple-service 'switch-to-tty2 shepherd-root-service-type
+                       (list (shepherd-service
+                              (provision '(kdb))
+                              (requirement '(virtual-terminal))
+                              (start #~(lambda ()
+                                         (invoke #$(file-append kbd "/bin/chvt") "2")))
+                              (respawn? #f))))
+       (service pcscd-service-type)
+       (screen-locker-service swaylock "swaylock")
+       (udev-rules-service
+	'backlight
+	(file->udev-rule "90-backlight.rules"
+			 (file-append light "/lib/udev/rules.d/90-backlight.rules"))))
+      (remove (lambda (service)
+		(member (service-kind service)
+			(list gdm-service-type
+			      screen-locker-service-type)))
+	      %desktop-services)))
 
-   (kernel-loadable-modules (list v4l2loopback-linux-module))
-   (services
-    (append
-     (list
-      ;; (simple-service
-      ;;  'add-xdg-desktop-portals
-      ;;  dbus-root-service-type
-      ;;  (list xdg-desktop-portal xdg-desktop-portal-wlr))
-      (simple-service 'switch-to-tty2 shepherd-root-service-type
-                      (list (shepherd-service
-                             (provision '(kdb))
-                             (requirement '(virtual-terminal))
-                             (start #~(lambda ()
-                                        (invoke #$(file-append kbd "/bin/chvt") "2")))
-                             (respawn? #f))))
-      (service pcscd-service-type)
-      (screen-locker-service swaylock "swaylock")
-      (udev-rules-service
-       'backlight
-       (file->udev-rule "90-backlight.rules"
-			(file-append light "/lib/udev/rules.d/90-backlight.rules"))))
-     (remove (lambda (service)
-	       (member (service-kind service)
-		       (list gdm-service-type
-			     screen-locker-service-type)))
-	     %desktop-services)))
-
-   ;; Allow resolution of '.local' host names with mDNS.
-   (name-service-switch %mdns-host-lookup-nss)))
+    ;; Allow resolution of '.local' host names with mDNS.
+    (name-service-switch %mdns-host-lookup-nss)))
 
 ;; (use-modules (guix store)
 ;; 	     (guix derivations))
