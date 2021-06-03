009e07fc52b546024eb36285eb2db4ffd94932f0
Andrew Tropin <andrew@trop.in>
2021-06-03 12:07:25 +0300

    rde: features: tmux: Add feature-tmux

---
+++ rde/features/tmux.scm
@@ -0,0 +1,34 @@
+(define-module (rde features tmux)
+  #:use-module (rde features)
+  #:use-module (rde features predicates)
+  #:use-module (gnu home-services base)
+  #:use-module (gnu services)
+  #:use-module (gnu packages tmux)
+  #:use-module (guix gexp)
+
+  #:export (feature-tmux))
+
+
+(define* (feature-tmux
+	  #:key
+	  config-file)
+  "Configure tmux."
+  (ensure-pred maybe-file-like? config-file)
+
+  (define (tmux-home-services config)
+    "Returns home services related to tmux."
+    ;; TODO: Implement home service and rewrite to it to make this
+    ;; feature extendable.
+    (list
+     (home-generic-service
+      'home-tmux
+      #:files
+      (filter list?
+	      (list (when config-file
+		      (list "config/tmux/tmux.conf" config-file))))
+      #:packages (list tmux))))
+
+  (feature
+   (name 'tmux)
+   (values `((tmux . #t)))
+   (home-services-getter tmux-home-services)))
