1dc801a8a483b4d05e9f1dde91665bdd5ef2153d
Andrew Tropin <andrew@trop.in>
2021-06-17 18:00:32 +0300

    gnu: home-services-utils: Remove unecessary \n before ini section

--- gnu/home-services-utils.scm
+++ gnu/home-services-utils.scm
@@ -303,7 +303,7 @@ DELIMITER interposed LS.  Support 'infix and 'suffix GRAMMAR values."
 (define (generic-serialize-ini-config-section section proc)
   "Format a section from SECTION for an INI configuration.
 Apply the procedure PROC on SECTION after it has been converted to a string"
-  (format #f "\n[~a]\n" (proc section)))
+  (format #f "[~a]\n" (proc section)))
 
 (define default-ini-format-section
   (match-lambda
