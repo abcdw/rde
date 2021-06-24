bcf2233ac7bba014e8f398a41254f3c7f04b1c36
Andrew Tropin <andrew@trop.in>
2021-06-24 11:10:53 +0300

    rde: features: emacs: Add smarter-beginning-of-line

--- rde/features/emacs.scm
+++ rde/features/emacs.scm
@@ -136,6 +136,35 @@
 	    ,#~""
 	    ;; (define-key global-map (kbd "M-/") 'hippie-expand)
 
+            ;; <https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/>
+            ;; Actually there is M-m for back-to-indentation
+            (defun smarter-move-beginning-of-line (arg)
+              "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+              (interactive "^p")
+              (setq arg (or arg 1))
+
+              ;; Move lines first
+              (when (/= arg 1)
+                (let ((line-move-visual nil))
+                  (forward-line (- arg 1))))
+
+              (let ((orig-point (point)))
+                (move-beginning-of-line 1)
+                (when (= orig-point (point))
+                  (back-to-indentation))))
+            ,#~"
+(define-key global-map
+  [remap move-beginning-of-line]
+  'smarter-move-beginning-of-line)\n"
+
 	    (column-number-mode 1)
 	    (save-place-mode 1)
 	    (show-paren-mode 1)
