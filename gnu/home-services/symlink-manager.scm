39c29f02826f10a2feccf32ebf65317e98838433
Andrew Tropin <andrew@trop.in>
2021-08-26 09:26:49 +0300

    gnu: symlink-manager: Add copyright info

--- gnu/home-services/symlink-manager.scm
+++ gnu/home-services/symlink-manager.scm
@@ -1,6 +1,24 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2021 Andrew Tropin <andrew@trop.in>
+;;; Copyright © 2021 Xinglu Chen <public@yoctocell.xyz>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (gnu home-services symlink-manager)
   #:use-module (gnu home-services)
-  #:use-module (gnu home-services shepherd)
   #:use-module (guix store)
   #:use-module (guix monads)
   #:use-module (guix gexp)
