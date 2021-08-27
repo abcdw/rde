d7706d7662847d215d94e064df912f05e191b628
Andrew Tropin <andrew@trop.in>
2021-08-27 08:07:09 +0300

    home-services: fontutils: Remove unused imports, add copyright info

--- gnu/home-services/fontutils.scm
+++ gnu/home-services/fontutils.scm
@@ -1,6 +1,24 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2021 Andrew Tropin <andrew@trop.in>
+;;; Copyright © 2021 Xinglu Chen <public@yoctocell.xyz>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (gnu home-services fontutils)
   #:use-module (gnu home-services)
-  #:use-module (gnu home-services files)
   #:use-module (gnu packages fontutils)
   #:use-module (guix gexp)
 
@@ -25,8 +43,6 @@
   <dir>~/.guix-home/profile/share/fonts</dir>
 </fontconfig>"))))
 
-;; TODO: fc-cache -f is too slow, it can be called only on-change or
-;; workarounded some other way.
 (define (regenerate-font-cache-gexp _)
   `(("profile/share/fonts"
      ,#~(system* #$(file-append fontconfig "/bin/fc-cache") "-fv"))))
@@ -43,7 +59,7 @@
                        (service-extension
                         home-profile-service-type
                         (const (list fontconfig)))))
-		(default-value #f)
+                (default-value #f)
                 (description
                  "Provides configuration file for fontconfig and make
 fc-* utilities aware of font packages installed in Guix Home's profile.")))
