71f944f48834f7764256590daf7508d9572b0c48
Andrew Tropin <andrew@trop.in>
2021-11-11 12:28:24 +0300

    rde: gnupg: Untabify.

--- rde/features/gnupg.scm
+++ rde/features/gnupg.scm
@@ -10,12 +10,12 @@
   #:export (feature-gnupg))
 
 (define* (feature-gnupg
-	  #:key gpg-primary-key
+          #:key gpg-primary-key
           (package gnupg)
-	  (gpg-ssh-agent? #t)
+          (gpg-ssh-agent? #t)
           (ssh-keys '())
-	  (pinentry-flavor 'qt)
-	  (gpg-smart-card? #f)
+          (pinentry-flavor 'qt)
+          (gpg-smart-card? #f)
           (default-ttl 86400)
           (gpg-extra-config '())
           (gpg-agent-extra-config '()))
@@ -36,9 +36,9 @@ and provides GPG-PRIMARY-KEY value for other features."
     (list
      (when (get-value 'sway config)
        (simple-service
-	'gnupg-updatestartuptty-on-sway-launch
-	home-sway-service-type
-	`((exec ,(file-append gnupg "/bin/gpg-connect-agent")
+        'gnupg-updatestartuptty-on-sway-launch
+        home-sway-service-type
+        `((exec ,(file-append gnupg "/bin/gpg-connect-agent")
                 updatestartuptty /bye >/dev/null)
           (,#~""))))
 
@@ -48,9 +48,9 @@ and provides GPG-PRIMARY-KEY value for other features."
       home-gnupg-service-type
       (home-gnupg-configuration
        (gpg-config
-	(home-gpg-configuration
-	 (extra-config
-	  `((keyid-format . long)
+        (home-gpg-configuration
+         (extra-config
+          `((keyid-format . long)
             (personal-cipher-preferences . (AES256 AES192 AES))
             (personal-digest-preferences . (SHA512 SHA384 SHA256))
             (personal-compress-preferences . (ZLIB BZIP2 ZIP Uncompressed))
@@ -62,9 +62,9 @@ and provides GPG-PRIMARY-KEY value for other features."
             (s2k-cipher-algo . AES256)
             (charset . utf-8)
 
-	    (with-subkey-fingerprint . #t)
+            (with-subkey-fingerprint . #t)
             (keyserver . "hkps://keyserver.ubuntu.com:443")
-	    ;; (keyserver . "hkps://keys.openpgp.org")
+            ;; (keyserver . "hkps://keys.openpgp.org")
             ;; (keyserver . "hkps://pgp.mit.edu")
             ;; (keyserver . "hkps://hkps.pool.sks-keyservers.net")
             ;; (keyserver . "hkps://ha.pool.sks-keyservers.net")
@@ -72,8 +72,8 @@ and provides GPG-PRIMARY-KEY value for other features."
 
             ,@gpg-extra-config))))
        (gpg-agent-config
-	(home-gpg-agent-configuration
-	 (extra-config
+        (home-gpg-agent-configuration
+         (extra-config
           ;; TTL for smart-cards doesn't make sense
           `(,@(if (not gpg-smart-card?)
                   `((default-cache-ttl . ,default-ttl)
@@ -82,23 +82,23 @@ and provides GPG-PRIMARY-KEY value for other features."
                     (max-cache-ttl-ssh . ,default-ttl))
                   '())
             ,@gpg-agent-extra-config))
-	 (ssh-agent? gpg-ssh-agent?)
+         (ssh-agent? gpg-ssh-agent?)
          (ssh-keys ssh-keys)
-	 (pinentry-flavor pinentry-flavor)))))))
+         (pinentry-flavor pinentry-flavor)))))))
 
   (define (system-gnupg-services _)
     "Return a list of home-services, required for gnupg to operate."
     (list
      (if gpg-smart-card?
-	 (service pcscd-service-type)
-	 #f)))
+         (service pcscd-service-type)
+         #f)))
 
   (feature
    (name 'gnupg)
    (values (append
-	    (make-feature-values gpg-primary-key gpg-ssh-agent?)
-	    (if gpg-ssh-agent?
-		'((ssh-agent? . #t))
-		'())))
+            (make-feature-values gpg-primary-key gpg-ssh-agent?)
+            (if gpg-ssh-agent?
+                '((ssh-agent? . #t))
+                '())))
    (home-services-getter home-gnupg-services)
    (system-services-getter system-gnupg-services)))
