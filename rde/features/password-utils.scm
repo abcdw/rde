171578fe276e6b051ab02249829b9cd319cda2a7
Demis Balbach <db@minikn.xyz>
2021-11-28 13:58:14 +0100

    rde: password-utils: Add pass.el emacs integration
    Hello,
    
    DISCLAIMER: I don't know if you have the need of something like
    this. But I always like using this with my password-store. Ultimately
    it's up to you if something like that should be merged, but I think some
    users may benefit from it.
    
    This patch adds integration of pass.el, password-store.el and
    password-store-otp.el IF `feature-emacs' is enabled.
    
    In addition, if `feature-completion' is enabled, it defines
    `rde/consult-pass', which allows to search the password-store using
    consult.
    
    Looking through your other emacs related features, I set the global
    keybinding to `M-g p' (p for password), that fits to how you bind stuff
    generally. But you have to decide that ultimately.
    
    I looked at DOOM's implementation of pass, which can be found here:
    https://github.com/hlissner/doom-emacs/tree/develop/modules/tools/pass
    
    `pass.el' and `password-store.el' offer a lot of function to fetch various things
    from the store, just run `M-x pass` then lookup functions like `M-x pass
    copy' and you'll find a lot of functions.
    
    DOOM defines some wrapper functions to make use of them, you can find
    them here:
    https://github.com/hlissner/doom-emacs/blob/develop/modules/tools/pass/autoload/pass.el
    
    But I don't know whats worth implementing and what not. I guess it would
    be nice to have function to fetch the username quickly. There is
    `pass-copy-username' but that only works if the field is called
    `username'. An alternative is `pass-copy-field' then interactively
    picking the one.
    
    Best regards,
    
    Demis Balbach

--- rde/features/password-utils.scm
+++ rde/features/password-utils.scm
@@ -1,8 +1,10 @@
 (define-module (rde features password-utils)
   #:use-module (rde features)
+  #:use-module (rde features emacs)
   #:use-module (rde features predicates)
   #:use-module (gnu home-services password-utils)
   #:use-module (gnu home-services state)
+  #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu services)
 
   #:export (feature-password-store))
@@ -13,6 +15,9 @@
 	  (remote-password-store-url #f))
   "Setup and configure password manager."
   ;; (ensure-pred maybe-url? remote-password-store-url)
+
+  (define emacs-f-name 'pass)
+  (define f-name (symbol-append 'emacs- emacs-f-name))
 
   (define (password-store-home-services config)
     "Returns home services related to password-store."
@@ -28,7 +33,42 @@
 	     (string-append
 	      (get-value 'home-directory config)
 	      "/.local/var/lib/password-store")
-	     remote-password-store-url)))))
+	     remote-password-store-url)))
+
+          ;; Configure emacs to use pass.el if feature-emacs is enabled.
+          (when (get-value 'emacs config)
+            (elisp-configuration-service
+             emacs-f-name
+             #:elisp-packages (list emacs-pass
+                                    emacs-password-store
+                                    emacs-password-store-otp)))
+
+          ;; Configure consult to search pass-store if feature-completion
+          ;; is enabled.
+          (when (get-value 'emacs-completion config)
+            (elisp-configuration-service
+             (symbol-append emacs-f-name '-completion)
+             `((define-key global-map (kbd "M-g p") 'rde-consult-pass)
+               (autoload 'password-store-dir "password-store")
+               (autoload 'password-store-list "password-store")
+
+               ;; Source:
+               ;; https://github.com/hlissner/doom-emacs/blob/develop/modules/tools/pass/autoload/consult.el
+               (defun rde-consult-pass (arg pass)
+                 "Interactively search the password store."
+                 (interactive
+                  (list current-prefix-arg
+                        (progn
+                         (require 'consult)
+                         (consult--read (password-store-list)
+                                        :prompt "Pass: "
+                                        :sort nil
+                                        :require-match t
+                                        :category 'pass))))
+                 (funcall (if arg
+                              'password-store-url
+                              'password-store-copy)
+                          pass)))))))
 
   (feature
    (name 'password-store)
