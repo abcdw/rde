36ebef082bb43519c3707b018307b203c9fb70db
Andrew Tropin <andrew@trop.in>
2021-11-29 14:11:58 +0300

    rde: packages: Add missing build-system import.

--- rde/packages.scm
+++ rde/packages.scm
@@ -13,6 +13,7 @@
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix build-system meson)
+  #:use-module (guix build-system trivial)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define (search-patch file-name)
