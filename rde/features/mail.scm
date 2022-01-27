43008a9a93e2b33e0942be3cac523472488403c8
Andrew Tropin <andrew@trop.in>
2022-01-27 14:06:09 +0300

    rde: notmuch: Add fn for opening messages and files in diff mode.

--- rde/features/mail.scm
+++ rde/features/mail.scm
@@ -753,6 +753,30 @@ not appear in the pop-up buffer."
                    ("d" rde-notmuch-trash-tags "Trash")
                    ("D" rde-notmuch-delete-tags "Delete")))
 
+           ;; https://notmuchmail.org/emacstips/#index25h2
+           (defun rde-notmuch-show-view-as-patch ()
+             "View the the current message as a patch."
+             (interactive)
+             (let* ((id (notmuch-show-get-message-id))
+                    (msg (notmuch-show-get-message-properties))
+                    (part (notmuch-show-get-part-properties))
+                    (subject (concat "Subject: " (notmuch-show-get-subject) "\n"))
+                    (diff-default-read-only t)
+                    (buf (get-buffer-create (concat "*notmuch-patch-" id "*")))
+                    (map (make-sparse-keymap)))
+               (define-key map "q" 'notmuch-bury-or-kill-this-buffer)
+               (switch-to-buffer buf)
+               (let ((inhibit-read-only t))
+                 (erase-buffer)
+                 (insert subject)
+                 (insert (notmuch-get-bodypart-text msg part nil)))
+               (set-buffer-modified-p nil)
+               (diff-mode)
+               (let ((new-ro-bind (cons 'buffer-read-only map)))
+                 (add-to-list 'minor-mode-overriding-map-alist new-ro-bind))
+               (goto-char (point-min))))
+           (define-key 'notmuch-show-part-map "d" 'rde-notmuch-show-view-as-patch)
+
            (define-key notmuch-search-mode-map "w"
              (lambda ()
                (interactive)
