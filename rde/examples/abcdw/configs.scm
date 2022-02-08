223d92f1c1cc96a9280aa35eee5425e1fb02942d
Samuel Culpepper <samuel@samuelculpepper.com>
2022-02-08 19:28:07 +0100

    qz: configs: use perfect-margin visible-width

--- rde/examples/abcdw/configs.scm
+++ rde/examples/abcdw/configs.scm
@@ -393,7 +393,8 @@
    (feature-emacs-input-methods)
    (feature-emacs-which-key)
    (feature-emacs-keycast)
-   (feature-emacs-perfect-margin)
+   (feature-emacs-perfect-margin
+    #:visible-width 150)
 
    (feature-emacs-dired)
    (feature-emacs-vterm)
