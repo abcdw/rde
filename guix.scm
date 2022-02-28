5f95b674ada6d6ea7320e7ec30b5e8a99071dd43
Andrew Tropin <andrew@trop.in>
2022-02-28 09:22:06 +0300

    Add guix.scm, update .envrc

---
+++ guix.scm
@@ -0,0 +1,10 @@
+(use-modules (guix gexp)
+             (guix packages)
+             (guix git-download)
+             (guix build-system trivial)
+             (rde packages)
+             (gnu packages base)
+             (gnu packages texinfo)
+             ((guix licenses) #:prefix license:))
+
+rde
