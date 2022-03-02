2a3459d833c6f1d7f5aaa0edeff802f706e57a43
Andrew Tropin <andrew@trop.in>
2022-03-02 08:38:00 +0300

    rde: packages: Add copyright note.

--- rde/packages.scm
+++ rde/packages.scm
@@ -1,3 +1,22 @@
+;;; rde --- Reproducible development environment.
+;;;
+;;; Copyright © 2021, 2022 Andrew Tropin <andrew@trop.in>
+;;;
+;;; This file is part of rde.
+;;;
+;;; rde is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; rde is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (rde packages)
   #:use-module (gnu packages)
   #:use-module (gnu packages linux)
@@ -9,6 +28,7 @@
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages image)
   #:use-module (gnu packages glib)
+  #:use-module (gnu packages base)
 
   #:use-module (srfi srfi-1)
 
@@ -155,8 +175,6 @@ keyboard input like xdotool type for X11.")
     (synopsis "Clipman")
     (description "GPL v3.0 2019- (C) yory8 <yory8@users.noreply.github.com>")
     (license license:gpl3)))
-
-(use-modules (gnu packages base))
 
 (define-public rde
   (package
