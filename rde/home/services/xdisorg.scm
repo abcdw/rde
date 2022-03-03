bd6943983737aff2301ed63ba6b4d8d3283fd3ea
Andrew Tropin <andrew@trop.in>
2022-03-03 09:31:09 +0300

    home: rofi: Use serialize-css for rasi config.

--- rde/home/services/xdisorg.scm
+++ rde/home/services/xdisorg.scm
@@ -18,6 +18,7 @@
 ;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (rde home services xdisorg)
+  #:use-module (rde serializers css)
   #:use-module (gnu home services)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu services configuration)
@@ -36,108 +37,7 @@
 (define rasi-config? list?)
 
 (define (serialize-rasi-config config)
-    (define (aligner nestness)
-    (apply string-append
-	   (map (const "    ") (iota nestness))))
-
-  (define (serialize-rasi-term term)
-    (match term
-      (#t "true")
-      (#f "false")
-      ((? symbol? e) (symbol->string e))
-      ((? number? e) (number->string e))
-      ((? string? e) (format #f "~s" e))
-      ((lst ...)
-       (raise (formatted-message
-               (G_ "Rasi term should be a non-list value (string, \
-boolean, number, symbol, or gexp). Provided term is:\n ~a") lst)))
-      (e e)))
-
-  (define* (serialize-rasi-pair
-	    expr #:optional (nestness 0))
-    (match expr
-      ((k)
-       (list (serialize-rasi-term k) ": " (serialize-rasi-term #t) ";\n"))
-      ((k . v)
-       (list (serialize-rasi-term k) ": " (serialize-rasi-term v) ";\n"))
-      (e
-       (raise (formatted-message
-               (G_ "Rasi pair should be a pair of terms, but provided \
-expression is:\n ~a") e)))))
-
-  (define* (serialize-rasi-properties
-	    properties #:optional (nestness 0))
-    (match properties
-      ;; properties:
-      ;; ((k1 . v1) (k2 . v2) (k3 . v3))
-      (((k . v) ...)
-       (append-map
-	(lambda (e)
-	  (append (list (aligner nestness))
-	          (serialize-rasi-pair e nestness)))
-	properties))
-      (e
-       (raise (formatted-message
-	       (G_ "Rasi properties should be a list of pairs, \
-but provided value is:\n ~a") e)))))
-
-  (define* (serialize-rasi-path-element p)
-    (if (list? p)
-        (append
-         (interpose (map serialize-rasi-term p) ", ")
-         (list " "))
-        (list (serialize-rasi-term p) " ")))
-
-  (define* (serialize-rasi-expression
-	    expr #:optional (nestness 0))
-
-    (match expr
-      ((p ((nested-p (e ...)) ...))
-       (append
-        (list (aligner nestness))
-        (serialize-rasi-path-element p)
-        (list "{\n")
-        (serialize-rasi-subconfig (cadr expr) (1+ nestness))
-        (list (aligner nestness))
-        (list "}\n")))
-      ((p ((k . v) ...))
-       (append
-        (list (aligner nestness))
-        (serialize-rasi-path-element p)
-        (list "{\n")
-        (serialize-rasi-properties (cadr expr) (1+ nestness))
-        (list (aligner nestness))
-        (list "}\n")))
-      (e
-       (raise (formatted-message
-               (G_ "Rasi expression should be a list of containing \
-path element as a first item and list of properties or subconfig as a \
-second is:\n ~a")
-               e)))))
-
-  (define* (serialize-rasi-subconfig
-	    subconfig #:optional (nestness 0))
-     (unless (list? subconfig)
-       (raise (formatted-message
-	       (G_ "Rasi (sub)config should be a gexp or a list of expressions, \
-but provided value is:\n ~a") subconfig)))
-     (append-map
-      (lambda (expr)
-        (match expr
-          ((? gexp? e) (list e))
-          (e (serialize-rasi-expression e nestness))))
-      subconfig))
-
-  (serialize-rasi-subconfig config))
-
-;; (display
-;;  (apply string-append
-;;         (serialize-rasi-config
-;;          '((configuration
-;;             ((timeout ((action . "kb-cancel")
-;;                        (delay . 0)))
-;;              ((a.g b) ((c . d)
-;;                        (e . f)))))))))
+  (serialize-css config))
 
 (define (rasi-config-file name config)
   (apply
@@ -152,11 +52,13 @@ but provided value is:\n ~a") subconfig)))
   (config-rasi
    (rasi-config
     '())
-   "The deepest element should be a list of pairs, which represents
-properties, intermediate items are path elements, it can be either a
-section name or a list containing a few section names, to represented
-nested sections use dot notation @code{section.subsection}, whitespace
-notation isn't supported.
+   "The deepest element should be a list of pairs, which represents properties,
+intermediate items are path elements, it can be either a section name or a
+list containing a few section names, to represented nested sections use dot
+notation @code{section.subsection}, or @code{(section subsection)}.
+
+For more details on serialization see documentation for @code{serialize-css}
+or @code{(rde serializers css)} module.
 
 See @code{man 5 rofi-theme} for more infromation about format and
 @code{man 1 rofi} for options.
@@ -165,8 +67,8 @@ See @code{man 5 rofi-theme} for more infromation about format and
 '((configuration
    ((timeout ((action . \"kb-cancel\")
               (delay . 0)))
-    ((a.g b) ((c . d)
-              (e . f))))))
+    (#(a.g b) ((c . d)
+               (e . f))))))
 @end lisp
 
 would yeild:
@@ -177,7 +79,8 @@ configuration {
         action: \"kb-cancel\";
         delay: 0;
     }
-    a.g, b {
+    a.g,
+    b {
         c: d;
         e: f;
     }
