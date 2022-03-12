18920c707daedd80e3cc86ee0f779266104305b9
Andrew Tropin <andrew@trop.in>
2022-03-12 08:02:26 +0300

    rde: examples: abcdw: Change sway-statusbar to waybar.

--- rde/examples/abcdw/configs.scm
+++ rde/examples/abcdw/configs.scm
@@ -195,8 +195,19 @@
    (feature-sway-run-on-tty
     #:sway-tty-number 2)
    (feature-sway-screenshot)
-   (feature-sway-statusbar
-    #:use-global-fonts? #f)
+   ;; (feature-sway-statusbar
+   ;;  #:use-global-fonts? #f)
+   (feature-waybar
+    #:waybar-modules
+    (list
+     (waybar-sway-workspaces)
+     ;; (waybar-sway-window)
+     (waybar-tray)
+     (waybar-idle-inhibitor)
+     ;; (waybar-temperature)
+     (waybar-sway-language)
+     (waybar-battery #:intense? #f)
+     (waybar-clock)))
    (feature-swayidle)
    (feature-swaylock
     #:swaylock (@ (gnu packages wm) swaylock-effects)
