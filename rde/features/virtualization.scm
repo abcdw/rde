3675d2ecbcec9957c15a12f28698756940ac3841
Andrew Tropin <andrew@trop.in>
2022-03-13 07:05:04 +0300

    rde: virtualization: Add feature-qemu.

---
+++ rde/features/virtualization.scm
@@ -0,0 +1,51 @@
+;;; rde --- Reproducible development environment
+;;; Copyright © 2022 Andrew Tropin <andrew@trop.in>
+;;;
+;;; This file is part of rde.
+;;;
+;;; rde is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; rde is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (rde features virtualization)
+  #:use-module (rde features)
+  #:use-module (gnu packages virtualization)
+  #:use-module (gnu services)
+  #:use-module (gnu home services)
+  #:use-module (rde system services accounts)
+
+  #:export (feature-qemu))
+
+(define* (feature-qemu
+          #:key
+          (qemu qemu))
+
+  (define f-name 'qemu)
+  (define (get-home-services config)
+    (list
+     (simple-service
+      'qemu-add-qemu-package
+      home-profile-service-type
+      (list qemu))))
+
+  (define (get-system-services config)
+    (list
+     (simple-service
+      'qemu-add-kvm-group-to-user
+      rde-account-service-type
+      (list "kvm"))))
+
+  (feature
+   (name f-name)
+   (values `((,f-name . #t)))
+   (home-services-getter get-home-services)
+   (system-services-getter get-system-services)))
