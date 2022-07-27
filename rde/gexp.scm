638f9faa773cb4de4a8bd9dad82d68a2dcc9c05d
Andrew Tropin <andrew@trop.in>
2022-07-27 10:09:21 +0300

    rde: gexp: Add template-file.

--- rde/gexp.scm
+++ rde/gexp.scm
@@ -22,7 +22,8 @@
   #:use-module (guix diagnostics)
   #:use-module (guix i18n)
 
-  #:export (slurp-file-like))
+  #:export (slurp-file-like
+            template-file))
 
 (define* (slurp-file-like file-like #:key (encoding "UTF-8"))
   "Returns a gexp, which reads all the content of the FILE-LIKE and returns it
@@ -34,3 +35,24 @@ as a string.  FILE-LIKE must be a file-like object."
   #~(call-with-input-file #$file-like
       (@ (ice-9 textual-ports) get-string-all)
       #:encoding #$encoding))
+
+(define* (template-file name file substitutes
+                        #:key (encoding "UTF-8"))
+  "Accepts file-like FILE and produces file-like NAME, where all entries matched
+with patterns from SUBSTITUTES, changed to corresponding values.  SUBSTITUTES
+is alist of regex patterns and values."
+  (mixed-text-file
+   name
+   #~(begin
+       (use-modules (ice-9 regex)
+                    (ice-9 textual-ports)
+                    (srfi srfi-1))
+       (fold-right
+        (lambda (pattern str)
+          (regexp-substitute/global
+           #f
+           (car pattern) str
+           'pre (cadr pattern) 'post))
+        (call-with-input-file #$file get-string-all #:encoding #$encoding)
+        '#$substitutes))))
+
