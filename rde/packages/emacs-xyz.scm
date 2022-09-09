ca438fd05c699872636a27e4325ed6cf0c268d11
Andrew Tropin <andrew@trop.in>
2022-09-09 14:34:51 +0300

    rde: packages: emacs-xyz: Add emacs-ol-notmuch.

--- rde/packages/emacs-xyz.scm
+++ rde/packages/emacs-xyz.scm
@@ -59,6 +59,29 @@
     "Paragraph justification for emacs using Knuth/Plass algorithm ")
    (license license:gpl3+))))
 
+(define-public emacs-ol-notmuch
+  (let* ((commit "1a53d6c707514784cabf33d865b577bf77f45913")
+         (revision "0"))
+    (package
+     (name "emacs-ol-notmuch")
+     (version (git-version "2.0.0" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.sr.ht/~tarsius/ol-notmuch")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "16p7j51z8r047alwn2hkb6944f7ds29ckb97b4k8ia00vwch0d67"))))
+     (build-system emacs-build-system)
+     (inputs (list emacs-org emacs-compat emacs-notmuch))
+     (home-page "https://git.sr.ht/~tarsius/ol-notmuch")
+     (synopsis "Links to notmuch messages and searches")
+     (description
+      "This package implements links to notmuch messages and searches. A search is a query to be performed by notmuch; it is the equivalent to folders in other mail clients. Similarly, mails are referred to by a query, so both a link can refer to several mails.")
+     (license license:gpl3+))))
+
 (define-public emacs-vertico-latest
   (let* ((commit "2de617a9199d152533ce280c6eb653147f15f8d1")
          (revision "2"))
