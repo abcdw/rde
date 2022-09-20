12276018ee8d3a210b23cbc67e2e1655a70c3e6b
Andrew Tropin <andrew@trop.in>
2022-09-20 15:21:43 +0300

    rde: password-store: Add copyright note.

--- rde/features/password-utils.scm
+++ rde/features/password-utils.scm
@@ -1,3 +1,23 @@
+;;; rde --- Reproducible development environment.
+;;;
+;;; Copyright © 2021, 2022 Andrew Tropin <andrew@trop.in>
+;;; Copyright © 2021 Demis Balbach <db@minikn.xyz>
+;;;
+;;; This file is part of rde.
+;;;
+;;; rde is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; rde is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (rde features password-utils)
   #:use-module (rde features)
   #:use-module (rde features emacs)
