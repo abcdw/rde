b06141484804d7a7f64aaa6f8121501610aeb93e
Andrew Tropin <andrew@trop.in>
2022-12-01 18:57:23 +0400

    system: networking: Add iwd-service-type.

--- rde/system/services/networking.scm
+++ rde/system/services/networking.scm
@@ -1,6 +1,7 @@
 ;;; rde --- Reproducible development environment.
 ;;;
 ;;; Copyright © 2022 Andrew Tropin <andrew@trop.in>
+;;; Copyright © 2021 Nikita Domnitskii
 ;;;
 ;;; This file is part of rde.
 ;;;
@@ -19,12 +20,15 @@
 
 (define-module (rde system services networking)
   #:use-module (rde serializers json)
+  #:use-module (rde serializers ini)
   #:use-module (gnu services)
   #:use-module (gnu services configuration)
   #:use-module (gnu services shepherd)
+  #:use-module (gnu services dbus)
   #:use-module (gnu packages networking)
   #:use-module (gnu packages admin)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages dns)
   #:use-module (gnu system shadow)
 
   #:use-module (guix packages)
@@ -35,7 +39,10 @@
   #:use-module (ice-9 match)
 
   #:export (yggdrasil-service-type
-            yggdrasil-configuration))
+            yggdrasil-configuration
+
+            iwd-service-type
+            iwd-configuration))
 
 
 ;;;
@@ -126,4 +133,62 @@
                 (description
                  "Run the Yggdrasil networking daemon.")))
 
+
+;;;
+;;; iwd.
+;;;
 
+(define-configuration/no-serialization iwd-configuration
+  (iwd (file-like iwd) "")
+  (openresolv (file-like openresolv) "")
+  (coreutils (file-like coreutils) "")
+  (config (ini-config '()) ""))
+
+(define (iwd-shepherd-service config)
+  "Return a shepherd service for iwd"
+  (let* ((iwd (iwd-configuration-iwd config))
+         (openresolv (iwd-configuration-openresolv config))
+         (coreutils (iwd-configuration-coreutils config))
+         (environment #~(list (string-append
+                               "PATH="
+                               (string-append #$openresolv "/sbin")
+                               ":"
+                               (string-append #$coreutils "/bin")))))
+    (list
+     (shepherd-service
+      (documentation "Run iwd")
+      (provision '(iwd))
+      (requirement '(user-processes dbus-system loopback))
+      (start #~(make-forkexec-constructor
+                (list (string-append #$iwd "/libexec/iwd"))
+                #:log-file "/var/log/iwd.log"
+                #:environment-variables #$environment))
+      (stop #~(make-kill-destructor))))))
+
+(define (iwd-etc-service config)
+  (let ((cfg (iwd-configuration-config config)))
+    `(("iwd/main.conf"
+       ,(apply mixed-text-file
+               "main.conf"
+               (serialize-ini-config cfg))))))
+
+(define iwd-package (compose list iwd-configuration-iwd))
+
+(define iwd-service-type
+  (service-type
+   (name 'iwd)
+   (extensions
+    (list (service-extension
+           shepherd-root-service-type
+           iwd-shepherd-service)
+          (service-extension
+           dbus-root-service-type
+           iwd-package)
+          (service-extension
+           etc-service-type
+           iwd-etc-service)
+          (service-extension
+           profile-service-type
+           iwd-package)))
+   (default-value (iwd-configuration))
+   (description "Configure and run iNet Wireless Daemon (iwd).")))
