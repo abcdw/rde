036c7fbf9aa603572bc6ee2857d3e23c22966464
Andrew Tropin <andrew@trop.in>
2022-12-02 20:45:15 +0400

    rde: gnupg: Disable shepherd? service.

--- rde/features/gnupg.scm
+++ rde/features/gnupg.scm
@@ -70,6 +70,7 @@ and provides GPG-PRIMARY-KEY value for other features."
       home-gnupg-service-type
       (home-gnupg-configuration
        (package gnupg)
+       (shepherd? #f)
        (gpg-config
         (home-gpg-configuration
          (extra-config
