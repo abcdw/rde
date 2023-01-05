d7e8950a1eb0e189455991fee2168577c411642d
Andrew Tropin <andrew@trop.in>
2023-01-05 12:55:58 +0400

    examples: Moves profiles related targets to separate Makefile.

---
+++ examples/profiles.mk
@@ -0,0 +1,34 @@
+
+#
+# Profiles
+#
+
+# Store items doesn't have useful mtime, so we rely on guix.lock to prevent
+# unecessary rebuilds
+guix: target/profiles/guix.lock
+
+target/profiles:
+	mkdir -p target/profiles
+
+target/profiles/guix.lock: rde/channels-lock.scm
+	make target/profiles/guix
+
+target/profiles/guix: target/profiles rde/channels-lock.scm
+	guix pull -C rde/channels-lock.scm -p ${GUIX_PROFILE} \
+	${PULL_EXTRA_OPTIONS}
+
+target/profiles/guix-local: target/profiles rde/channels-lock-local.scm
+	guix pull -C rde/channels-lock-local.scm -p ${GUIX_PROFILE} \
+	${PULL_EXTRA_OPTIONS}
+
+rde/channels-lock.scm: rde/channels.scm
+	echo -e "(use-modules (guix channels))\n" > ./rde/channels-lock-tmp.scm
+	guix time-machine -C ./rde/channels.scm -- \
+	describe -f channels >> ./rde/channels-lock-tmp.scm
+	mv ./rde/channels-lock-tmp.scm ./rde/channels-lock.scm
+
+rde/channels-lock-local.scm: rde/channels-local.scm
+	echo -e "(use-modules (guix channels))\n" > ./rde/channels-lock-tmp.scm
+	guix time-machine -C ./rde/channels-local.scm -- \
+	describe -f channels >> ./rde/channels-lock-tmp.scm
+	mv ./rde/channels-lock-tmp.scm ./rde/channels-lock-local.scm
