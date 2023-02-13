42c0f5b0b59582aa40ca2b5574d8881886630c29
Andrew Tropin <andrew@trop.in>
2023-02-13 13:27:02 +0400

    tests: Add module with define-test macro.

---
+++ tests/rde/tests.scm
@@ -0,0 +1,32 @@
+;;; rde --- Reproducible development environment.
+;;;
+;;; Copyright © 2023 Andrew Tropin <andrew@trop.in>
+;;;
+;;; This file is part of rde.
+;;;
+;;; rde is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; rde is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (rde tests)
+  #:export (define-test test?))
+
+(define-syntax define-test
+  (syntax-rules ()
+    ((_ test-name e ...)
+     (begin
+       (define-public (test-name) e ...)
+       (set-procedure-property! test-name 'srfi-64-test? #t)))))
+
+(define (test? proc)
+  "Checks if PROC is a test."
+  (and (procedure? proc) (procedure-property proc 'srfi-64-test?)))
