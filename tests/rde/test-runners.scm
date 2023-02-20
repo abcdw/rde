fedacfdeb6dcd6696080dd73996832a16f66409a
Andrew Tropin <andrew@trop.in>
2023-02-20 13:14:49 +0400

    tests: Specify the report message of a test runner.

--- tests/rde/test-runners.scm
+++ tests/rde/test-runners.scm
@@ -53,7 +53,7 @@
           (else #t))))
     (test-runner-on-final! runner
       (lambda (runner)
-        (format #t "Source: ~a\npass = ~a, xfail = ~a, fail = ~a\n\n"
+        (format #t "Source: ~a\nAsserts: pass = ~a, xfail = ~a, fail = ~a\n\n"
                 (test-result-ref runner 'source-file)
                 (test-runner-pass-count runner)
                 (test-runner-xfail-count runner)
