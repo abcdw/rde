7035634cb5505f33b7abd9ea2617e3ddd12ec34b
Andrew Tropin <andrew@trop.in>
2023-09-12 16:06:07 +0400

    rde: emacs: Remove wayland clipboard fix.
    It's not needed anymore.

--- src/rde/features/emacs.scm
+++ src/rde/features/emacs.scm
@@ -214,37 +214,6 @@ Prefix keymap for binding various minor modes for toggling functionalitty.")
    #:autoloads? #t
    #:summary "Additional autoloaded rde startup configuration."))
 
-(define (wayland-clipboard-fix config)
-  (let* ((wl-cb (get-value 'wl-clipboard config wl-clipboard))
-         (wl-copy (file-append wl-cb "/bin/wl-copy"))
-         (wl-paste (file-append wl-cb "/bin/wl-paste"))
-
-         (cu-fallback (get-value 'coreutils config coreutils-minimal))
-         (cu-min (get-value 'coreutils-minimal config cu-fallback))
-         (tr (file-append cu-min "/bin/tr")))
-    `((setq wl-copy-process nil)
-      (setq wl-copy-binary ,wl-copy)
-      (setq wl-paste-binary ,wl-paste)
-      (setq tr-binary ,tr)
-      (setq wl-paste-command
-            (format "%s -n | %s -d \r" wl-paste-binary tr-binary))
-
-      (defun wl-copy (text)
-        (setq wl-copy-process (make-process :name "wl-copy"
-                                            :buffer nil
-                                            :command `(,wl-copy-binary "-n")
-                                            :connection-type 'pipe))
-        (process-send-string wl-copy-process text)
-        (process-send-eof wl-copy-process))
-
-      (defun wl-paste ()
-        (if (and wl-copy-process (process-live-p wl-copy-process))
-            nil ; should return nil if we're the current paste owner
-            (shell-command-to-string wl-paste-command)))
-
-      (setq interprogram-cut-function 'wl-copy)
-      (setq interprogram-paste-function 'wl-paste))))
-
 (define (rde-emacs-base config)
   (let* ((full-name (get-value 'full-name config))
          (email     (get-value 'email config)))
@@ -543,11 +512,7 @@ It can contain settings not yet moved to separate features."
           ,#~""
           ;; FIXME: Move it back to the configure-rde-emacs package, when it
           ;; will be built with emacs-29
-          (pixel-scroll-precision-mode 1)
-
-          ,@(if (get-value 'wayland config)
-                (wayland-clipboard-fix config)
-                '())))
+          (pixel-scroll-precision-mode 1)))
          ;;; TODO: Rebuilding packages with emacs will be useful for
          ;;; native-comp, but some packages fails to build, need to fix them.
        (rebuild-elisp-packages? #f)))
