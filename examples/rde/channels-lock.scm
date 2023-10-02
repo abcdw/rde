86406bcb39bc89291124feff2010a36f234b31e6
Andrew Tropin <andrew@trop.in>
2023-10-02 14:57:13 +0400

    examples: channels-lock: Update 2023-10-02

--- examples/rde/channels-lock.scm
+++ examples/rde/channels-lock.scm
@@ -5,7 +5,7 @@
         (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
         (commit
-          "f5b3af3b8f1702032fc52a2f9cc574fa1b713136")
+          "16fec2068ec03bf2420188edd9840eaa241fbcf7")
         (introduction
           (make-channel-introduction
             "257cebd587b66e4d865b3537a9a88cccd7107c95"
@@ -16,7 +16,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "07d43c66d5c11fef61f9846fefb97fa18e4764f1")
+          "15fdeaeb2d4df755811db92cc7c3c7cb19155dec")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
