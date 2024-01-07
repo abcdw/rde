7fc40c585bea0af3f69894962ec269742235c3de
Andrew Tropin <andrew@trop.in>
2024-01-07 14:04:12 +0300

    rde: youtube-dl: Add Miguel's copyright

--- src/rde/features/video.scm
+++ src/rde/features/video.scm
@@ -1,6 +1,7 @@
 ;;; rde --- Reproducible development environment.
 ;;;
 ;;; Copyright © 2021, 2022, 2023, 2024 Andrew Tropin <andrew@trop.in>
+;;; Copyright © 2023 Miguel Ángel Moreno <mail@migalmoreno.com>
 ;;;
 ;;; This file is part of rde.
 ;;;
