5359b23ea9005187ca9e77e2e1404ae8349df37f
Andrew Tropin <andrew@trop.in>
2024-04-22 08:52:41 +0300

    examples: channels-lock: Update 2024-04-14

--- examples/rde/channels-lock.scm
+++ examples/rde/channels-lock.scm
@@ -1,24 +1,24 @@
 (use-modules (guix channels))
 
 (list (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
-        (branch "master")
-        (commit
-          "72ae8364e4846172453c4ec05656bda2b495f2e3")
-        (introduction
-          (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
-            (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "5f86eebd240958001ab4f178005f355d24d9b7f1")
+          "4d67912265aa921084040d633238ca30c7dc4199")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
+        (branch "master")
+        (commit
+          "915583314b838f1e0f8dc4e5ce77936c0082cf4e")
+        (introduction
+          (make-channel-introduction
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            (openpgp-fingerprint
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
