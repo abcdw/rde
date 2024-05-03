9895f042e32c67d196e63e8af9754936451e6888
Andrew Tropin <andrew@trop.in>
2024-05-03 11:08:45 +0300

    gnu: Use upstreamed version of guix-home-service-type

--- src/gnu/services/home.scm
+++ src/gnu/services/home.scm
@@ -1,47 +1,12 @@
 (define-module (gnu services home)
-  #:use-module (gnu services)
-  #:use-module (gnu services shepherd)
+  #:use-module (gnu services guix)
 
-  #:use-module (guix gexp)
-  #:use-module (guix packages)
-  #:use-module (guix records)
+  #:use-module (guix deprecation)
 
   #:export (guix-home-service-type))
 
-
-(define (guix-home-shepherd-service config)
-  (map
-   (lambda (x)
-     (let ((user (car x))
-           (he (cdr x)))
-       (shepherd-service
-        (documentation "Activate Guix Home.")
-        ;; Originally requirement was user-homes, but for recently it stopped
-        ;; working, seems like it was executed too early and didn't work, so
-        ;; we switched to term-tty1.
-        (requirement '(term-tty1))
-        (provision (list (symbol-append 'guix-home- (string->symbol user))))
-        (one-shot? #t)
-        (auto-start? #t)
-        (start #~(make-forkexec-constructor
-                  '(#$(file-append he "/activate"))
-                  #:user #$user
-                  #:environment-variables
-                  (list (string-append "HOME=" (passwd:dir (getpw #$user))))
-                  #:group (group:name (getgrgid (passwd:gid (getpw #$user))))))
-        (stop #~(make-kill-destructor)))))
-     config))
-
-(define (guix-home-gc-roots config)
-  (map cdr config))
-
-(define guix-home-service-type
-  (service-type
-   (name 'guix-home)
-   (description "Setups home-environments specified in the value.")
-   (extensions (list (service-extension
-                      shepherd-root-service-type
-                      guix-home-shepherd-service)))
-   ;; (compose append)
-   ;; (extend append)
-   (default-value '())))
+;; Use upstreamed version of the service:
+;; https://yhetil.org/guix-patches/05fd930c91838ce9640720ce68e3379f10154590.1710355300.git.richard@freakingpenguin.com/
+(define-deprecated/alias
+  guix-home-service-type
+  (@ (gnu services guix) guix-home-service-type))
