77d694e02833e3db39498a3770dc1da855e28bb1
Andrew Tropin <andrew@trop.in>
2024-05-07 09:34:33 +0300

    rde: notmuch: Add TODO about bug-reference

--- src/rde/features/mail.scm
+++ src/rde/features/mail.scm
@@ -1475,7 +1475,11 @@ not appear in the pop-up buffer."
 
 ;; TODO: revisit this package
 ;; https://github.com/larkery/emacs/blob/master/site-lisp/notmuch-attachment-list.el
-;; https://michal.sapka.me/2023/notmuch/
+;; https://michal.sapka.me/2023/notmuch
+
+;; TODO: [Andrew Tropin, 2024-05-07] Add bug-reference support, similiar to
+;; bug-reference-setup-from-mail-alist, extandable from feature-sourcehut,
+;; feature-guix and others
 (define* (feature-notmuch
           #:key
           (notmuch notmuch)
