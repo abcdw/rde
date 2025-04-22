20efed11e0f2d29cf4e2fc35cd2e2b19e95889ee
Noé Lopez <noelopez@free.fr>
2025-04-22 10:14:30 +0200

    packages: guile-xyz: Add guile-ares-shepherd
    * src/rde/packages/guile-xyz.scm (guile-ares-shepherd): New package.

--- src/rde/packages/guile-xyz.scm
+++ src/rde/packages/guile-xyz.scm
@@ -18,9 +18,13 @@
 ;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (rde packages guile-xyz)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages guile)
   #:use-module (gnu packages guile-xyz)
   #:use-module (guix packages)
-  #:use-module (guix git-download))
+  #:use-module (guix git-download)
+  #:use-module (guix build-system guile)
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define-public guile-ares-rs-latest
   (let* ((commit "24d0832251fbb1b44fb3adf4fa1a085e0c396e77")
@@ -39,3 +43,29 @@
          (sha256
           (base32
            "01wa8l9piwjmq2xhpgblfp8h0yq45hqa4ys9ddfia3d5rp081n45")))))))
+
+(define-public guile-ares-shepherd
+  (package
+    (name "guile-ares-shepherd")
+    (version "0.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://codeberg.org/cons-town/guile-debugger")
+                    (commit (string-append "ares-shepherd-" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1abnrp67zph4cc8x4pmmja52ma5ixf9ilcz13r19ga8ar8b36hv0"))))
+    (build-system guile-build-system)
+    (arguments
+     (list #:source-directory "shepherd-nrepl/src/guile"))
+    (native-inputs `(("guile" ,guile-next)))
+    (inputs (list shepherd-1.0
+                  guile-fibers
+                  guile-ares-rs-latest))
+    (home-page "https://codeberg.org/cons-town/guile-debugger")
+    (synopsis "Shepherd interface for Ares")
+    (description "ares-shepherd is an extension for Ares that adds the ability to
+connect and interact to a shepherd via its nREPL service.")
+    (license license:gpl3+)))
