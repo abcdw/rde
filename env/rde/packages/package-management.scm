0e0b5baf07692558939fdad81e220d685d3f68b0
Andrew Tropin <andrew@trop.in>
2025-05-22 08:14:02 +0700

    packages: Add make-guix-package, make-channels-package

--- env/rde/packages/package-management.scm
+++ env/rde/packages/package-management.scm
@@ -29,7 +29,9 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (rde lib file)
-  #:export (guix-from-channels-lock)
+  #:export (guix-from-channels-lock
+            make-guix-package
+            make-channels-package)
   #:declarative? #f)
 
 (define (get-guix-channel channels)
@@ -39,11 +41,13 @@
 (define my-channels
   (load (canonicalize-path (find-file-in-load-path "channels-lock.scm"))))
 
-(define-public guix-from-channels-lock
-  (let ((commit (channel-commit (get-guix-channel my-channels))))
+(define (make-guix-package channels)
+  (let ((commit (channel-commit (get-guix-channel channels))))
     (package
       (inherit guix)
-      (version (string-append "1.4.0-" (string-take commit 7)))
+      (version
+       (string-append (car (string-split (package-version guix) #\-))
+                      "-" (string-take commit 7)))
       (source
        (git-checkout
         (url "https://codeberg.org/guix/guix-mirror")
@@ -58,9 +62,83 @@
           #~(append
              #$flags
              (list
-              ;; "--disable-daemon"
-              ;; "--localstatedir=/var"
               #$(string-append "--with-channel-commit=" commit))))))
 
       (inputs (modify-inputs (package-inputs guix)
                 (replace "guile" guile-next))))))
+
+(define guix-from-channels-lock
+  (make-guix-package my-channels))
+
+(define (channel->git-checkout channel)
+  (git-checkout
+   (url (channel-url channel))
+   (commit (channel-commit channel))))
+
+(define* (channels-union name channels
+                         #:key
+                         (quiet? #f)
+                         (resolve-collision 'resolve-collision/default))
+  "Return a directory that is the union of CHANNELS sources."
+  (define log-port
+    (if quiet?
+        (gexp (%make-void-port "w"))
+        (gexp (current-error-port))))
+
+  (computed-file
+   name
+   (with-imported-modules '((guix build union))
+     (gexp
+      (begin
+        (use-modules (guix build union)
+                     (srfi srfi-1)) ;for 'first' and 'last'
+
+        (define (thing->srcs thing)
+          (with-input-from-file (string-append thing "/.guix-channel")
+            (lambda ()
+              (let ((dirs (assoc-ref (cdr (read)) 'directory)))
+                (if dirs
+                    (map (lambda (x) (string-append thing "/" x)) dirs)
+                    (list thing))))))
+
+        (union-build (ungexp output)
+                     (append-map thing->srcs '#$channels)
+
+                     #:log-port (ungexp log-port)
+                     #:symlink symlink
+                     #:resolve-collision
+                     (ungexp resolve-collision)))))))
+
+(define (channels->combined-source-code channels)
+  (channels-union
+   "channels-sources"
+   (map channel->git-checkout channels)))
+
+(define (make-channels-package channels)
+  (package
+    (name "channels")
+    (version "0.1.0")
+    (source (channels->combined-source-code
+             (remove guix-channel? channels)))
+    (build-system guile-build-system)
+    (arguments
+     (list
+      #:source-directory "."
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'unpack
+            (lambda* (#:key source #:allow-other-keys)
+              (mkdir "source")
+              (chdir "source")
+              (copy-recursively source "."
+                                #:keep-mtime? #t
+                                #:follow-symlinks? #t)
+              (for-each (lambda (f)
+                          (false-if-exception (make-file-writable f)))
+                        (find-files ".")))))))
+    (inputs `(("guile" ,guile-next)
+              ("guix" ,(guix-from-channels channels))))
+    (home-page "https://git.sr.ht/~abcdw/rde")
+    (synopsis "Combined package for channel source and bytecode files")
+    (description "Combined package for channel source and bytecode files.")
+    (license license:gpl3+)))
