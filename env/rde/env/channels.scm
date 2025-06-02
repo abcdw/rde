e1d5d4309ae03382dd5b255099b5c8b021e5572e
Andrew Tropin <andrew@trop.in>
2025-06-02 09:25:42 +0700

    env: Update SPDX

--- env/rde/env/channels.scm
+++ env/rde/env/channels.scm
@@ -1,3 +1,9 @@
+;;; rde --- Reproducible development environment.
+;;;
+;;; SPDX-FileCopyrightText: 2024, 2025 Andrew Tropin <andrew@trop.in>
+;;;
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
 (define-module (rde env channels)
   #:use-module (guix channels)
   #:export (core-channels))
--- env/rde/env/packages.scm
+++ env/rde/env/packages.scm
@@ -1,21 +1,9 @@
 ;;; rde --- Reproducible development environment.
 ;;;
-;;; Copyright © 2024, 2025 Andrew Tropin <andrew@trop.in>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;;
-;;; This file is part of rde.
-;;;
-;;; rde is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
-;;;
-;;; rde is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with rde.  If not, see <http://www.gnu.org/licenses/>.
+;;; SPDX-FileCopyrightText: 2024, 2025 Andrew Tropin <andrew@trop.in>
+
 
 (define-module (rde env packages)
   #:use-module (rde lib file)
