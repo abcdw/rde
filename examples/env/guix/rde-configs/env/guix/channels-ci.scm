861997d3e339a85d0e5b2a5c98e15000c735c23a
Andrew Tropin <andrew@trop.in>
2025-06-04 10:14:00 +0700

    Move channels-ci to examples

--- examples/env/guix/rde-configs/env/guix/channels-ci.scm
+++ examples/env/guix/rde-configs/env/guix/channels-ci.scm
@@ -4,9 +4,9 @@
 ;;;
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
-(define-module (rde env guix channels-ci)
+(define-module (rde-configs env guix channels-ci)
   #:use-module (guix channels)
-  #:use-module (rde env guix channels)
+  #:use-module ((rde env guix channels) #:prefix rde:)
   #:use-module (srfi srfi-1))
 
 (define core-channels-with-local-rde
@@ -20,6 +20,6 @@
       "257cebd587b66e4d865b3537a9a88cccd7107c95"
       (openpgp-fingerprint
        "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-   core-channels))
+   rde:core-channels))
 
 core-channels-with-local-rde
