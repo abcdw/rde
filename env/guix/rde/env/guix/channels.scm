0a3b62f08611da5e5d0987cd75ef7c0d49878bf8
Andrew Tropin <andrew@trop.in>
2025-06-16 17:37:01 +0700

    env: Update guix channel 2025-06-16

--- env/guix/rde/env/guix/channels.scm
+++ env/guix/rde/env/guix/channels.scm
@@ -14,7 +14,7 @@
          (url "https://git.guix.gnu.org/guix.git")
          (branch "master")
          (commit
-          "0be64e5fc441b1e302fd6b78ee6eed2d04d870b3")
+          "ced31f8dd156e4202a2c7115fc003608a541388c")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
