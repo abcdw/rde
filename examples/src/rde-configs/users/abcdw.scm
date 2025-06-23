d1651b8c8af13889a2aa4cf645b4a0f1f32ebc0b
Andrew Tropin <andrew@trop.in>
2025-06-23 07:00:19 +0700

    examples: abcdw: Remove bogus newline

--- examples/src/rde-configs/users/abcdw.scm
+++ examples/src/rde-configs/users/abcdw.scm
@@ -266,8 +266,7 @@
     (mpv-conf
      `((global
         ((keep-open . yes)
-         (ytdl-format . "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best
-")
+         (ytdl-format . "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best")
          (save-position-on-quit . yes)
          (speed . 1.61))))))))
 
