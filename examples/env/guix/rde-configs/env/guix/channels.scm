edd7d33078616adb58501b722951fe93e5980522
Andrew Tropin <andrew@trop.in>
2025-06-24 17:49:46 +0700

    examples: env: Update rde channel

--- examples/env/guix/rde-configs/env/guix/channels.scm
+++ examples/env/guix/rde-configs/env/guix/channels.scm
@@ -16,7 +16,7 @@
     (url "https://git.sr.ht/~abcdw/rde")
     (branch "master")
     (commit
-     "5669a5f2e729635746bd9015e50bd9e3abe14b3a")
+     "cffc90375f49241ae1ced1d0548e36a300e75e96")
     (introduction
      (make-channel-introduction
       "257cebd587b66e4d865b3537a9a88cccd7107c95"
