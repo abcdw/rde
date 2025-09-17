0112e78be148702eadf4c75477b841b145f647b9
Andrew Tropin <andrew@trop.in>
2025-09-17 13:29:31 +0700

    env, examples: Update guix channel 2025-09-17

--- examples/env/guix/rde/env/guix/channels.scm
+++ examples/env/guix/rde/env/guix/channels.scm
@@ -14,7 +14,7 @@
          (url "https://git.guix.gnu.org/guix.git")
          (branch "master")
          (commit
-          "48fea09d68d575e82c986c93785786165bd95f82")
+          "92c63391ee25205be3b8525d5d1fe5b9f345f37f")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
