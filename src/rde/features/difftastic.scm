7c554b3580994c3565bfb51957a9c7a78ca0f43a
Andrew Tropin <andrew@trop.in>
2026-02-27 19:36:00 +0700

    features: difftastic: Add Emacs integration

--- src/rde/features/difftastic.scm
+++ src/rde/features/difftastic.scm
@@ -1,6 +1,8 @@
 (define-module (rde features difftastic)
   #:use-module (rde features)
+  #:use-module (rde features emacs)
   #:use-module (rde predicates)
+  #:use-module (rde packages emacs-xyz)
   #:use-module (gnu home-services version-control)
   #:use-module (gnu home services)
   #:use-module (gnu services)
@@ -11,9 +13,11 @@
 (define* (feature-difftastic
           #:key
           (difftastic difftastic)
+          (emacs-difftastic emacs-difftastic)
           (parse-error-limit #f))
   "Setup and configure Difftastic, a structural diff tool."
   (ensure-pred file-like? difftastic)
+  (ensure-pred file-like? emacs-difftastic)
   (ensure-pred maybe-integer? parse-error-limit)
 
   (define f-name 'difftastic)
@@ -40,9 +44,21 @@
       (home-git-extension
        (config
         `((diff
-           ((external . ,difft-cmd)))))))))
+           ((external . ,difft-cmd)))))))
+
+     (rde-elisp-configuration-service
+      'difftastic
+      config
+      `((setopt difftastic-executable ,difft-cmd)
+        (with-eval-after-load 'magit
+          (difftastic-bindings-mode)))
+      #:summary "Difftastic structural diff integration"
+      #:commentary "Set executable path and enable magit keybindings."
+      #:keywords '(convenience)
+      #:elisp-packages (list emacs-difftastic))))
 
   (feature
    (name f-name)
-   (values `((,f-name . ,difftastic)))
+   (values `((,f-name . ,difftastic)
+             (emacs-difftastic . ,emacs-difftastic)))
    (home-services-getter get-home-services)))
