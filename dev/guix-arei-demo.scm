9ac9b38b915209c2dbb50c0395a8621eb7d387d5
Andrew Tropin <andrew@trop.in>
2026-03-05 18:28:54 +0700

    dev: guix-arei-demo: Refactor and extend demo examples

--- dev/guix-arei-demo.scm
+++ dev/guix-arei-demo.scm
@@ -17,11 +17,36 @@
   #:use-module (rde features base)
   #:use-module (rde features terminals)
   #:use-module (rde features fontutils)
+  #:use-module (guix packages)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages base)
   #:use-module (rde api store))
 
 ;; TODO: [Andrew Tropin, 2026-03-02] Use eval to comment for demo
 
+(define (rde-get-config-file config file-name)
+  "Build and return the store path for FILE-NAME from CONFIG's
+home xdg configuration files."
+  (car
+   (build
+    (car
+     (assoc-ref
+      (service-value
+       (fold-services
+        (instantiate-missing-services
+         (cons* (service home-xdg-configuration-files-service-type '())
+                (service home-profile-service-type '())
+                (rde-config-home-services config)))
+        #:target-type home-xdg-configuration-files-service-type))
+      file-name)))))
+
+(define (rde-get-profile config)
+  (build-derivation
+   (profile-derivation
+    (packages->manifest (rde-config-home-packages config)))))
+
 (define (comment)
+  ;; 1. Simple packages
   (build htop)
   (build foot)
 
@@ -29,10 +54,56 @@
    (string-append (car (build foot)) "/bin/foot")
    (string-append (car (build htop)) "/bin/htop"))
 
+  ;; 1.5 gexps evaluated in isolated environment
+
+  (evaluate-gexp
+   #~(begin
+       (use-modules (ice-9 ftw))
+       (display (getcwd))
+       (newline)
+       (for-each
+        (lambda (f) (display f) (display " \n"))
+        (scandir (getcwd)))
+       (newline)
+       ;; (system* #$(file-append coreutils "/bin/touch") #$output)
+       ;; (exit 10)
+       ))
+
+  (evaluate-gexp
+   #~(begin
+       ;; (system* "touch" "output.txt")
+       (symlink #$(file-append htop "/bin/htop") #$output)
+
+       (system* #$(file-append coreutils "/bin/pwd"))
+       (system* #$(file-append coreutils "/bin/ls") "-lia")
+       (system* #$(file-append coreutils "/bin/ls") "/")
+       ;; (exit 1)
+       #$output))
+
+
+  ;; 2. Customizing packages
+  (define foot-gcc-14
+    (package
+      (inherit foot)
+      (native-inputs
+       (modify-inputs (package-native-inputs foot)
+         (prepend gcc-14)))))
+
+  foot
+  foot-gcc-14
+
+  (build foot)
+  (build foot-gcc-14)
+
+  ;; multiple versions of the same library
+  ;; /usr/lib/library-a.so
+
   (define dev-profile
     (build-derivation
      (profile-derivation
       (packages->manifest (list foot htop python python-pyfiglet)))))
+  dev-profile
+  ;; => "/gnu/store/hxinsrkg2r6hh4i6qzn0688wjg7rlvzr-profile"
 
   (system*
    "bash" "-c"
@@ -40,6 +111,8 @@
                   dev-profile "/bin/foot"))
 
   ;; python3 -c "from pyfiglet import figlet_format; print(figlet_format('Guix'))"
+
+  ;; Polylang, Reproducible Development Environments
 
   (define features
     (list
@@ -54,29 +127,20 @@
   (pretty-print-rde-config config)
 
   (define demo-profile
-    (build-derivation
-     (profile-derivation
-      (packages->manifest (rde-config-home-packages config)))))
+    (rde-get-profile config))
 
+  (build foot)
+  ;; => ("/gnu/store/8fhrfk1y4whkvi34qvn76c3grx48zhw0-foot-1.25.0")
   (define foot-config
-    (car
-     (build
-      (car
-       (assoc-ref
-        (service-value
-         (fold-services
-          (instantiate-missing-services
-           (cons* (service home-xdg-configuration-files-service-type '())
-                  (service home-profile-service-type '())
-                  (rde-config-home-services config)))
-          #:target-type home-xdg-configuration-files-service-type))
-        "foot/foot.ini")))))
+          ;; => "/gnu/store/jn9di4iw048v0xmvxc1v7yjmhlb05rrz-foot.ini"
+    (rde-get-config-file config "foot/foot.ini"))
 
   (system*
    "bash" "-c"
-   (string-append "source " demo-profile "/etc/profile && exec "
-                  demo-profile "/bin/foot"
-                  " --config " foot-config))
+   (string-append
+    "source " (rde-get-profile config) "/etc/profile && exec "
+    "foot" " --config " (rde-get-config-file config "foot/foot.ini")))
 
   ;; Make alacritty and foot use the same colorscheme
+
   )
