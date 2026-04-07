277691fc143efdb032ed834076a84ae7afcfdea5
Andrew Tropin <andrew@trop.in>
2026-04-07 09:38:46 +0700

    features: jujutsu: Add config and set name and email

--- src/rde/features/jujutsu.scm
+++ src/rde/features/jujutsu.scm
@@ -9,7 +9,9 @@
   #:use-module (rde features emacs)
   #:use-module (rde predicates)
   #:use-module (rde packages emacs-xyz)
+  #:use-module (rde serializers ini)
   #:use-module (gnu home services)
+  #:use-module (gnu home services xdg)
   #:use-module (gnu services)
   #:use-module (gnu packages rust-apps)
   #:use-module (guix gexp)
@@ -18,19 +20,43 @@
 (define* (feature-jujutsu
           #:key
           (jujutsu jujutsu)
-          (emacs-majutsu emacs-majutsu))
-  "Setup and configure Jujutsu version control system with Emacs integration."
+          (emacs-majutsu emacs-majutsu)
+          (extra-config '()))
+  "Setup and configure Jujutsu version control system with Emacs integration.
+
+EXTRA-CONFIG is an alist of additional INI/TOML sections appended to the
+generated @file{~/.config/jj/config.toml}."
   (ensure-pred file-like? jujutsu)
   (ensure-pred file-like? emacs-majutsu)
+  (ensure-pred list? extra-config)
 
   (define f-name 'jujutsu)
 
   (define (get-home-services config)
+    (define full-name (get-value 'full-name config))
+    (define email    (get-value 'email config))
+    (define emacs-client (get-value 'emacs-client config #f))
+
+    (define jj-config
+      `((user
+         ((name  . ,full-name)
+          (email . ,email)))
+        ,@extra-config))
+
     (list
      (simple-service
       'add-jujutsu-home-package
       home-profile-service-type
       (list jujutsu))
+     (simple-service
+      'jujutsu-config
+      home-xdg-configuration-files-service-type
+      `(("jj/config.toml"
+         ,(apply mixed-text-file
+                 "jj-config.toml"
+                 (ini-serialize
+                  jj-config
+                  #:equal-string " = ")))))
      (rde-elisp-configuration-service
       f-name
       config
